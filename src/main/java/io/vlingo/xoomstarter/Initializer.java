a5460e224379b6cee9f53ac8ae81ca95313c04e8
Danilo Ambrosio <danilo.ambrosio@gmail.com>
2020-04-01 23:44:28 -0300

    Requires context

--- src/main/java/io/vlingo/xoomstarter/Initializer.java
+++ src/main/java/io/vlingo/xoomstarter/Initializer.java
@@ -1,5 +1,6 @@
 package io.vlingo.xoomstarter;
 
+import io.vlingo.xoomstarter.template.TemplateGenerationContext;
 import io.vlingo.xoomstarter.template.TemplateGenerator;
 
 public class Initializer {
@@ -7,7 +8,7 @@ public class Initializer {
     private static final String ROOT_FOLDER = "user.dir";
 
     public static void main(final String[] args) {
-        TemplateGenerator.start();
+        TemplateGenerator.start(new TemplateGenerationContext());
     }
 
 }
