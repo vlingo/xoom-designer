308996b86b894bcfa1f208e530822c46989c34a8
Danilo Ambrosio <danilo.ambrosio@gmail.com>
2020-04-01 23:48:01 -0300

    Cleaning

--- src/main/java/io/vlingo/xoomstarter/template/steps/ArchetypeResolverStep.java
+++ src/main/java/io/vlingo/xoomstarter/template/steps/ArchetypeResolverStep.java
@@ -5,17 +5,9 @@ import io.vlingo.xoomstarter.template.TemplateGenerationContext;
 
 public class ArchetypeResolverStep implements TemplateGenerationStep  {
 
-    private static final String ARCHETYPE_CMD = "mvn archetype:generate -B " +
-            "-DarchetypeGroupId=org.apache.maven.archetypes " +
-            "-DarchetypeArtifactId=maven-archetype-quickstart " +
-            "-DarchetypeVersion=1.1 " +
-            "-DgroupId=io.vlingo " +
-            "-DartifactId=starter-example " +
-            "-Dversion=1.0-SNAPSHOT " +
-            "-Dpackage=io.vlingo.starterexample";
-
     @Override
     public void process(final TemplateGenerationContext context) {
         context.selectArchetype(Archetype.DEFAULT);
     }
+
 }
