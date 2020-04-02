fdca1ea02f3f7b7e9425a3eba3d87e6f7f143d10
Danilo Ambrosio <danilo.ambrosio@gmail.com>
2020-04-02 11:03:13 -0300

    Fix Command

--- src/main/java/io/vlingo/xoomstarter/template/steps/DefaultCommandExecutorStep.java
+++ src/main/java/io/vlingo/xoomstarter/template/steps/DefaultCommandExecutorStep.java
@@ -3,7 +3,7 @@ package io.vlingo.xoomstarter.template.steps;
 public class DefaultCommandExecutorStep extends CommandExecutorStep {
 
     private static final String UNSUPPORTED_OPERATING_SYSTEM = "Windows";
-    private static final String COMMAND_PATTERN = "bash -c cd %s && %s";
+    private static final String COMMAND_PATTERN = "sh -c cd %s && %s";
 
     @Override
     protected String commandPattern() {
