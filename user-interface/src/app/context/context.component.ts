3f0c9e34f3bf35405afcf66e91699b95e4572d64
Danilo Ambrosio <danilo.ambrosio@gmail.com>
2020-04-20 17:41:25 -0300

    Test

---
+++ user-interface/src/app/context/context.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-context',
+  templateUrl: './context.component.html',
+  styleUrls: ['./context.component.css']
+})
+export class ContextComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
