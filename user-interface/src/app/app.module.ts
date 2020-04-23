35fb315872165abe72a9189867bd4f12aa179c2e
Danilo Ambrosio <danilo.ambrosio@gmail.com>
2020-04-23 17:02:08 -0300

    Main structure (HTML, CSS, JS)

--- user-interface/src/app/app.module.ts
+++ user-interface/src/app/app.module.ts
@@ -1,22 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LeftMenuComponent } from './left-menu/left-menu.component';
-import { ContextComponent } from './context/context.component';
+import { ContextComponent } from './steps/context/context.component';
+import { ModelComponent } from './steps/model/model.component';
+import { TableComponent } from './steps/model/table/table.component';
+import { DeploymentComponent } from './steps/deployment/deployment.component';
+import { GenerationComponent } from './steps/generation/generation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LeftMenuComponent,
-    ContextComponent
+    ContextComponent,
+    ModelComponent,
+    TableComponent,
+    DeploymentComponent,
+    GenerationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
