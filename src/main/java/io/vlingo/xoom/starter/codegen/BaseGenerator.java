c0bfc36eec1a9a38fabbe249a94c45e1561688dd
VaughnVernon <vaughn@forcomprehension.com>
2020-04-29 00:36:13 -0700

    Refactored to generation class hierarchy.

---
+++ src/main/java/io/vlingo/xoom/starter/codegen/BaseGenerator.java
@@ -0,0 +1,50 @@
+// Copyright © 2012-2020 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.xoom.starter.codegen;
+
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateExceptionHandler;
+
+public abstract class BaseGenerator {
+  protected final Configuration configuration;
+  protected final Map<String, Object> input;
+
+  public BaseGenerator() {
+    this.configuration = configure();
+    this.input = new HashMap<>();
+  }
+
+  public void generate(final String templateName) throws Exception {
+    final Template template = configuration.getTemplate("codegen/" + templateName + ".ftl");
+
+    final Writer consoleWriter = new OutputStreamWriter(System.out);
+
+    template.process(input, consoleWriter);
+  }
+
+  public void inputOfPackageName(final String packageName) {
+    input.put("packageName", packageName);
+  }
+
+  private Configuration configure() {
+    final Configuration configuration = new Configuration(Configuration.DEFAULT_INCOMPATIBLE_IMPROVEMENTS);
+    configuration.setClassForTemplateLoading(this.getClass(), "/");
+    configuration.setDefaultEncoding("UTF-8");
+    configuration.setLocale(Locale.US);
+    configuration.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+
+    return configuration;
+  }
+}
