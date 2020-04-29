08934b77e70490d416888dc2098b8694e0f4de70
VaughnVernon <vaughn@forcomprehension.com>
2020-04-29 00:36:13 -0700

    Refactored to generation class hierarchy.

---
+++ src/main/java/io/vlingo/xoom/starter/codegen/ImportsGenerator.java
@@ -0,0 +1,26 @@
+// Copyright © 2012-2020 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.xoom.starter.codegen;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class ImportsGenerator extends BaseGenerator {
+  protected final List<ImportHolder> importsHolder;
+
+  public ImportsGenerator() {
+    super();
+
+    this.importsHolder = new ArrayList<>();
+    this.input.put("imports", importsHolder);
+  }
+
+  public void inputOf(final ImportHolder importHolder) {
+    importsHolder.add(importHolder);
+  }
+}
