eb9f154e854b83d19360e9644d4534e702eb73c6
Danilo Ambrosio <danilo.ambrosio@gmail.com>
2020-04-28 22:41:28 -0300

    About

--- user-interface/src/app/app.component.ts
+++ user-interface/src/app/app.component.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
-import { StepCompletion } from './model/step-completion';
-import { Step, StepPath, StepName } from './model/step';
-import { Router } from '@angular/router';
-import { StepComponent } from './steps/step.component';
-import { GenerationSettings } from './model/generation-settings';
-import { ToastrService } from 'ngx-toastr';
+import { Router, NavigationEnd } from '@angular/router';
+import { RoutingHistoryService } from './routing/routing-history.service';
 
 @Component({
   selector: 'app-root',
@@ -13,82 +9,10 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class AppComponent {
 
-  activeComponent: StepComponent;
-  generationSettings: GenerationSettings;
-  stepStatus = new Map<Step, Boolean>();
-
-  constructor(private router: Router, private toastrService: ToastrService) {
-    this.router.navigate(['/context']);
-    this.generationSettings = new GenerationSettings();
-  }
-
-  handleStepCompletion(stepCompletion: StepCompletion) {
-   
-    this.stepStatus.set(stepCompletion.step, stepCompletion.valid);
-   
-    if(stepCompletion.requiresNavigation()) {
-      let path = StepPath.get(stepCompletion.nextStep());
-      this.router.navigate([path]);
-    } 
-
-    if(stepCompletion.isGenerationRequested()) {
-      this.validateAndSubmit();
+  constructor(private router: Router, private routingHistoryService: RoutingHistoryService) {
+    if(routingHistoryService.isFirstAccess()) {
+      this.router.navigate(['/settings/context']);
     }
-  }
-
-  validateAndSubmit() {
-    let invalidSteps = new Array<String>();
-    
-    this.stepStatus.forEach((value: Boolean, key: Step) => {
-      if(!value) {
-        invalidSteps.push(StepName.get(key));
-      }
-    });
-
-    if(invalidSteps.length != 0) {
-      this.toastrService.warning("Please inform the required settings in the following step(s): " + invalidSteps.join(", "));
-    }
-  }
-
-  onComponentActivated(componentReference: StepComponent) {
-    this.activeComponent = componentReference;
-    this.activeComponent.generationSettings = this.generationSettings; 
-    this.activeComponent.stepCompletion.subscribe((stepCompletion: StepCompletion) => {
-      this.handleStepCompletion(stepCompletion);
-    })
-  }
-
-  next() {
-    this.activeComponent.next();
-  }
-
-  previous() {
-    this.activeComponent.previous();
-  }
-
-  generate() {
-    this.activeComponent.generate();
-  }
-
-  hasNext() {
-    if(this.activeComponent == undefined) {
-      return false;
-    }
-    return this.activeComponent.hasNext();
-  }
-
-  hasPrevious() {
-    if(this.activeComponent == undefined) {
-      return false;
-    }  
-    return this.activeComponent.hasPrevious();
-  }
-
-  isLastStep() {
-    if(this.activeComponent == undefined) {
-      return false;
-    }
-    return this.activeComponent.isLastStep();
   }
   
 }
--- user-interface/src/app/model/model-settings.ts
+++ user-interface/src/app/model/model-settings.ts
@@ -2,6 +2,7 @@ import { Aggregate } from './aggregate';
 import { DomainEvent } from './domain-event';
 
 export class ModelSettings {
+
     
     public aggregates = new Array<Aggregate>();
     public restResources = new Array<Aggregate>();
@@ -13,13 +14,17 @@ export class ModelSettings {
     }
 
     public domainEventsOf(aggregateId: Number) : Array<DomainEvent> {
-        const aggregate = this.aggregates.find(aggregate => aggregateId === aggregate.id);
+        const aggregate = this.aggregateOfId(aggregateId);
 
         if(aggregate == undefined) {
             return new Array<DomainEvent>();
         }
 
         return aggregate.events;
+    }
+
+    public aggregateOfId(aggregateId: Number) : Aggregate {
+        return this.aggregates.find(aggregate => aggregateId === aggregate.id);
     }
 
 }
