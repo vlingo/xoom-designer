585634e91e93c4c7b8e73a8a36a28487a023f0cc
VaughnVernon <vaughn@forcomprehension.com>
2020-04-29 00:36:13 -0700

    Refactored to generation class hierarchy.

--- src/main/java/io/vlingo/xoom/starter/codegen/CommandModelStateStoreProviderGenerator.java
+++ src/main/java/io/vlingo/xoom/starter/codegen/CommandModelStateStoreProviderGenerator.java
@@ -7,75 +7,26 @@
 
 package io.vlingo.xoom.starter.codegen;
 
-import java.io.OutputStreamWriter;
-import java.io.Writer;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
-import java.util.Map;
 
-import freemarker.template.Configuration;
-import freemarker.template.Template;
-import freemarker.template.TemplateExceptionHandler;
-
-public class CommandModelStateStoreProviderGenerator {
-  private final Configuration configuration;
-  private final Map<String, Object> input;
-  private final List<ImportHolder> importsHolder;
-  private final List<StateAdapterHolder> stateAdaptersHolder;
+public class CommandModelStateStoreProviderGenerator extends ImportsGenerator {
+  protected final List<StateAdapterHolder> stateAdaptersHolder;
 
   public CommandModelStateStoreProviderGenerator() {
-    this.configuration = configure();
-
-    this.input = new HashMap<>();
-
-    this.importsHolder = new ArrayList<>();
-    this.input.put("imports", importsHolder);
+    super();
 
     this.stateAdaptersHolder = new ArrayList<>();
     input.put("stateAdapters", stateAdaptersHolder);
   }
 
-  public void generate() throws Exception {
-    final Template template = configuration.getTemplate("codegen/CommandModelStateStoreProvider.ftl");
-
-    final Writer consoleWriter = new OutputStreamWriter(System.out);
-
-    template.process(input, consoleWriter);
-  }
-
-  public CommandModelStateStoreProviderGenerator inputOf(final ImportHolder importHolder) {
-    importsHolder.add(importHolder);
-
-    return this;
-  }
-
-  public CommandModelStateStoreProviderGenerator inputOf(final StateAdapterHolder stateAdapterHolder) {
+  public void inputOf(final StateAdapterHolder stateAdapterHolder) {
     stateAdaptersHolder.add(stateAdapterHolder);
-
-    return this;
-  }
-
-  public CommandModelStateStoreProviderGenerator inputOfPackageName(final String packageName) {
-    input.put("packageName", packageName);
-
-    return this;
   }
 
   public CommandModelStateStoreProviderGenerator inputOfStateStoreClassName(final String stateStoreClassName) {
     input.put("stateStoreClassName", stateStoreClassName);
 
     return this;
-  }
-
-  private Configuration configure() {
-    final Configuration configuration = new Configuration(Configuration.DEFAULT_INCOMPATIBLE_IMPROVEMENTS);
-    configuration.setClassForTemplateLoading(this.getClass(), "/");
-    configuration.setDefaultEncoding("UTF-8");
-    configuration.setLocale(Locale.US);
-    configuration.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
-
-    return configuration;
   }
 }
