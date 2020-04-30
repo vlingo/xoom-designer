6770490beb3fe109807fa18985498ce086d789b3
VaughnVernon <vaughn@forcomprehension.com>
2020-04-29 23:55:14 -0700

    Print String from generate().

--- src/test/java/io/vlingo/xoom/starter/codegen/GeneratorsTest.java
+++ src/test/java/io/vlingo/xoom/starter/codegen/GeneratorsTest.java
@@ -13,7 +13,7 @@ public class GeneratorsTest {
 
   @Test
   public void testCommandModelStateStoreProviderGenerator() throws Exception {
-    final CommandModelStateStoreProviderGenerator generator = new CommandModelStateStoreProviderGenerator();
+    final StateStoreProviderGenerator generator = new StateStoreProviderGenerator();
 
     generator.inputOfPackageName("com.beyond5.auth.infra.persistence");
 
@@ -28,7 +28,30 @@ public class GeneratorsTest {
     generator.inputOf(new StateAdapterHolder("UserState"));
     generator.inputOf(new StateAdapterHolder("ProfileState"));
 
-    generator.generate("CommandModelStateStoreProvider");
+    System.out.println(generator.generate("CommandModelStateStoreProvider"));
+  }
+
+  @Test
+  public void testQueryModelStateStoreProviderGenerator() throws Exception {
+    final StateStoreProviderGenerator generator = new StateStoreProviderGenerator();
+
+    generator.inputOfPackageName("com.beyond5.auth.infra.persistence");
+
+    generator.inputOf(new ImportHolder("com.beyond5.auth.model.profile.ProfileState"));
+    generator.inputOf(new ImportHolder("com.beyond5.auth.model.tenant.TenantState"));
+    generator.inputOf(new ImportHolder("com.beyond5.auth.model.user.UserState"));
+
+    // TODO: Needs ActorInstantiator
+    generator.inputOfStateStoreClassName("io.vlingo.symbio.store.journal.jdbc.JDBCStateStoreActor");
+
+    generator.inputOf(new StateAdapterHolder("TenantState"));
+    generator.inputOf(new StateAdapterHolder("UserState"));
+    generator.inputOf(new StateAdapterHolder("ProfileState"));
+
+    // TODO: Needs pre-generated Queries and QueriesActor for, but with no methods:
+    //       final Queries queries = stage.actorFor(Queries.class, QueriesActor.class, store);
+
+    System.out.println(generator.generate("QueryModelStateStoreProvider"));
   }
 
   @Test
@@ -36,65 +59,65 @@ public class GeneratorsTest {
     final StateAdapterGenerator profileStateGenerator = new StateAdapterGenerator("ProfileState");
     profileStateGenerator.inputOfPackageName("com.beyond5.auth.infra.persistence");
     profileStateGenerator.inputOf(new ImportHolder("com.beyond5.auth.model.profile.ProfileState"));
-    profileStateGenerator.generate("StateAdapter");
+    System.out.println(profileStateGenerator.generate("StateAdapter"));
 
     final StateAdapterGenerator tenantStateGenerator = new StateAdapterGenerator("TenantState");
     tenantStateGenerator.inputOfPackageName("com.beyond5.auth.infra.persistence");
     tenantStateGenerator.inputOf(new ImportHolder("com.beyond5.auth.model.tenant.TenantState"));
-    tenantStateGenerator.generate("StateAdapter");
+    System.out.println(tenantStateGenerator.generate("StateAdapter"));
 
     final StateAdapterGenerator userStateGenerator = new StateAdapterGenerator("UserState");
     userStateGenerator.inputOfPackageName("com.beyond5.auth.infra.persistence");
     userStateGenerator.inputOf(new ImportHolder("com.beyond5.auth.model.user.UserState"));
-    userStateGenerator.generate("StateAdapter");
+    System.out.println(userStateGenerator.generate("StateAdapter"));
   }
 
   @Test
   public void testBeanState() throws Exception {
     final StateGenerator valueStateGenerator = new StateGenerator("ProfileState");
     valueStateGenerator.inputOfPackageName("com.beyond5.auth.model.profile");
-    valueStateGenerator.generate("BeanState");
+    System.out.println(valueStateGenerator.generate("BeanState"));
   }
 
   @Test
   public void testValueState() throws Exception {
     final StateGenerator valueStateGenerator = new StateGenerator("UserState");
     valueStateGenerator.inputOfPackageName("com.beyond5.auth.model.user");
-    valueStateGenerator.generate("ValueState");
+    System.out.println(valueStateGenerator.generate("ValueState"));
   }
 
   @Test
   public void testDomainEvent() throws Exception {
     final DomainEventGenerator domainEventGenerator = new DomainEventGenerator("TenantRegistered");
     domainEventGenerator.inputOfPackageName("com.beyond5.auth.model.tenant");
-    domainEventGenerator.generate("DomainEvent");
+    System.out.println(domainEventGenerator.generate("DomainEvent"));
   }
 
   @Test
   public void testPlaceholderDefinedEvent() throws Exception {
     final DomainEventGenerator domainEventGenerator = new PlaceholderDefinedEventGenerator("Tenant");
     domainEventGenerator.inputOfPackageName("com.beyond5.auth.model.tenant");
-    domainEventGenerator.generate("PlaceholderDefinedEvent");
+    System.out.println(domainEventGenerator.generate("PlaceholderDefinedEvent"));
   }
 
   @Test
   public void testEventSourcedAggregate() throws Exception {
     final AggregateGenerator aggregateGenerator = new AggregateGenerator("Tenant", "TenantState");
     aggregateGenerator.inputOfPackageName("com.beyond5.auth.model.tenant");
-    aggregateGenerator.generate("EventSourcedEntity");
+    System.out.println(aggregateGenerator.generate("EventSourcedEntity"));
   }
 
   @Test
   public void testObjectAggregate() throws Exception {
     final AggregateGenerator aggregateGenerator = new AggregateGenerator("Profile", "ProfileState");
     aggregateGenerator.inputOfPackageName("com.beyond5.auth.model.profile");
-    aggregateGenerator.generate("ObjectEntity");
+    System.out.println(aggregateGenerator.generate("ObjectEntity"));
   }
 
   @Test
   public void testStatefulAggregate() throws Exception {
     final AggregateGenerator aggregateGenerator = new AggregateGenerator("User", "UserState");
     aggregateGenerator.inputOfPackageName("com.beyond5.auth.model.user");
-    aggregateGenerator.generate("StatefulEntity");
+    System.out.println(aggregateGenerator.generate("StatefulEntity"));
   }
 }
