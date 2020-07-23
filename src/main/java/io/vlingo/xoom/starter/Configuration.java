8d607b3797f741601d8cdf8f8065883455d838be
Danilo Ambrosio <danilo.ambrosio@gmail.com>
2020-07-23 20:46:58 -0300

    Refactoring Impacts
    Former-commit-id: ecc3921402c50a694975f96e78b3abb69035232f
    Former-commit-id: 4582a70b8638e77df031f9a336fb9faf2d268030

--- src/main/java/io/vlingo/xoom/starter/Configuration.java
+++ src/main/java/io/vlingo/xoom/starter/Configuration.java
@@ -3,15 +3,23 @@ package io.vlingo.xoom.starter;
 import com.google.common.collect.Maps;
 import io.vlingo.http.ResponseHeader;
 import io.vlingo.http.media.ContentMediaType;
-import io.vlingo.xoom.codegen.steps.*;
-import io.vlingo.xoom.codegen.steps.CodeGenerationStep;
+import io.vlingo.xoom.codegen.CodeGenerationStep;
+import io.vlingo.xoom.codegen.content.ContentCreationStep;
+import io.vlingo.xoom.codegen.template.bootstrap.BootstrapGenerationStep;
+import io.vlingo.xoom.codegen.template.model.ModelGenerationStep;
+import io.vlingo.xoom.codegen.template.projections.ProjectionGenerationStep;
+import io.vlingo.xoom.codegen.template.resource.RestResourceGenerationStep;
+import io.vlingo.xoom.codegen.template.storage.StorageGenerationStep;
 import io.vlingo.xoom.starter.task.gui.steps.BrowserLaunchCommandResolverStep;
 import io.vlingo.xoom.starter.task.gui.steps.UserInterfaceBootstrapStep;
 import io.vlingo.xoom.starter.task.steps.*;
 import io.vlingo.xoom.starter.task.template.Terminal;
 import io.vlingo.xoom.starter.task.template.steps.*;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Configuration {
 
