8ed6fd891051bcf934a767d9c17c07fd426396aa
Danilo Ambrosio <danilo.ambrosio@gmail.com>
2020-08-04 23:28:27 -0300

    Persistence annotation and related

--- user-interface/src/app/model/context-settings.ts
+++ user-interface/src/app/model/context-settings.ts
@@ -5,8 +5,8 @@ export class ContextSettings {
     public artifactVersion: String;
     public packageName: String;
     public xoomVersion: String;
-    
+
     constructor(){
-        this.xoomVersion = "1.3.0";
+        this.xoomVersion = "1.3.4-SNAPSHOT";
     }
 }
