34e8ba85e61b1dbe4dfbb6bdcb95566482de5c03
Danilo Ambrosio <danilo.ambrosio@gmail.com>
2020-10-08 16:53:13 -0300

    Agent handling improvement

--- src/main/java/io/vlingo/xoom/starter/task/projectgeneration/steps/CodeGenerationExecutionerStep.java
+++ src/main/java/io/vlingo/xoom/starter/task/projectgeneration/steps/CodeGenerationExecutionerStep.java
@@ -7,6 +7,7 @@
 package io.vlingo.xoom.starter.task.projectgeneration.steps;
 
 import io.vlingo.xoom.codegen.CodeGenerationContext;
+import io.vlingo.xoom.codegen.parameter.CodeGenerationParameters;
 import io.vlingo.xoom.starter.task.TaskExecutionContext;
 import io.vlingo.xoom.starter.task.steps.TaskExecutionStep;
 
@@ -16,19 +17,18 @@ public class CodeGenerationExecutionerStep implements TaskExecutionStep {
 
     @Override
     public void process(final TaskExecutionContext taskExecutionContext) {
-        final CodeGenerationContext context =
-                resolveContext(taskExecutionContext);
-
+        final CodeGenerationParameters parameters = resolveParameters(taskExecutionContext);
+        final CodeGenerationContext context = CodeGenerationContext.with(parameters);
         CODE_GENERATION_STEPS.stream()
                 .filter(step -> step.shouldProcess(context))
                 .forEach(step -> step.process(context));
     }
 
-    private CodeGenerationContext resolveContext(final TaskExecutionContext taskExecutionContext) {
-        if(taskExecutionContext.hasCodeGenerationParameters()) {
-            return CodeGenerationContext.with(taskExecutionContext.codeGenerationParameters());
+    private CodeGenerationParameters resolveParameters(final TaskExecutionContext taskExecutionContext) {
+        if(taskExecutionContext.agent().isTerminal()) {
+            return CodeGenerationParametersMapper.of(taskExecutionContext);
         }
-        return CodeGenerationContext.with(CodeGenerationParametersMapper.of(taskExecutionContext));
+        return taskExecutionContext.codeGenerationParameters();
     }
 
 }
