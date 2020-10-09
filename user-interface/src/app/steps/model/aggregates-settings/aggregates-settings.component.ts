ddf26b6516394441ebd0f64f0d54352ff0225b54
Raphael Alves <rsaraphael@gmail.com>
2020-10-09 17:57:32 -0300

    Add card to see aggregates

--- user-interface/src/app/steps/model/aggregates-settings/aggregates-settings.component.ts
+++ user-interface/src/app/steps/model/aggregates-settings/aggregates-settings.component.ts
@@ -1,4 +1,4 @@
-import { ModelAggregate } from './../../../model/model-aggregate';
+import { ModelAggregate, AggregatesSetting } from './../../../model/model-aggregate';
 import {
   FormArray,
   FormGroup,
@@ -15,68 +15,6 @@ export interface PeriodicElement {
   weight: number;
   symbol: string;
 }
-
-const ELEMENT_DATA: PeriodicElement[] = [{
-    position: 1,
-    name: 'Hydrogen',
-    weight: 1.0079,
-    symbol: 'H'
-  },
-  {
-    position: 2,
-    name: 'Helium',
-    weight: 4.0026,
-    symbol: 'He'
-  },
-  {
-    position: 3,
-    name: 'Lithium',
-    weight: 6.941,
-    symbol: 'Li'
-  },
-  {
-    position: 4,
-    name: 'Beryllium',
-    weight: 9.0122,
-    symbol: 'Be'
-  },
-  {
-    position: 5,
-    name: 'Boron',
-    weight: 10.811,
-    symbol: 'B'
-  },
-  {
-    position: 6,
-    name: 'Carbon',
-    weight: 12.0107,
-    symbol: 'C'
-  },
-  {
-    position: 7,
-    name: 'Nitrogen',
-    weight: 14.0067,
-    symbol: 'N'
-  },
-  {
-    position: 8,
-    name: 'Oxygen',
-    weight: 15.9994,
-    symbol: 'O'
-  },
-  {
-    position: 9,
-    name: 'Fluorine',
-    weight: 18.9984,
-    symbol: 'F'
-  },
-  {
-    position: 10,
-    name: 'Neon',
-    weight: 20.1797,
-    symbol: 'Ne'
-  },
-];
 
 @Component({
   selector: 'app-aggregates-settings',
@@ -95,17 +33,11 @@ export class AggregatesSettingsComponent implements OnInit {
     methods: this.createMethods,
     api: this.createApi
   };
+  aggregatesSettings: AggregatesSetting[] = [];
 
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
-    this.aggregateSettingsForm = this.formBuilder.group({
-      aggregateName: ['', [Validators.required]],
-      stateFields: this.formBuilder.array([this.createStateField(this.formBuilder)]),
-      events: this.formBuilder.array([this.createEvents(this.formBuilder)]),
-      methods: this.formBuilder.array([this.createMethods(this.formBuilder)]),
-      api: this.formBuilder.array([this.createApi(this.formBuilder)])
-    });
   }
 
   removeRow(type: string, index: number): void {
@@ -130,11 +62,20 @@ export class AggregatesSettingsComponent implements OnInit {
 
   enableNewAggregate(flag: boolean){
     this.enableAdd = flag;
+    if (flag) {
+      this.aggregateSettingsForm = this.formBuilder.group({
+        aggregateName: ['', [Validators.required]],
+        stateFields: this.formBuilder.array([this.createStateField(this.formBuilder)]),
+        events: this.formBuilder.array([this.createEvents(this.formBuilder)]),
+        methods: this.formBuilder.array([this.createMethods(this.formBuilder)]),
+        api: this.formBuilder.array([this.createApi(this.formBuilder)])
+      });
+    }
   }
 
   add(){
-    const model = this.aggregateSettingsForm.value as ModelAggregate;
-    console.log(model);
+    this.aggregatesSettings.push(this.aggregateSettingsForm.value as AggregatesSetting);
+    this.enableAdd = false;
   }
 
   addNewRow(type: string){
@@ -180,8 +121,4 @@ export class AggregatesSettingsComponent implements OnInit {
       })],
     });
   }
-
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
-
 }
