738411aa45ac72619ab4ca56254ee5e87b065f40
Raphael Alves <rsaraphael@gmail.com>
2020-10-12 15:25:33 -0300

    Add model Service
    Former-commit-id: d45052d9e0ac43bf4b23d32ca0f52dca77420baf
    Former-commit-id: 81ec00e7866f9387b0808dcc0f356ab15b34967a

--- user-interface/src/app/steps/generation/generation.component.ts
+++ user-interface/src/app/steps/generation/generation.component.ts
@@ -1,3 +1,4 @@
+import { ModelService } from './../model/model.service';
 import { Component, OnInit } from '@angular/core';
 import { StepComponent } from '../step.component';
 import { StepCompletion } from 'src/app/model/step-completion';
@@ -14,7 +15,7 @@ export class GenerationComponent extends StepComponent {
 
   generationForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private modelService: ModelService) {
     super();
     // this.createForm("gaba-hey");
   }
@@ -29,6 +30,7 @@ export class GenerationComponent extends StepComponent {
   }
 
   generate() {
+    this.modelService.getModel$.subscribe(a => console.log(a));
     this.stepCompletion.emit(new StepCompletion(
       Step.GENERATION,
       this.generationForm.valid,
