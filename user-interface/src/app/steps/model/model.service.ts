b5a00ff59d0529d90801b1b38dfc8b94464e4417
Raphael Alves <rsaraphael@gmail.com>
2020-10-12 15:25:33 -0300

    Add model Service

---
+++ user-interface/src/app/steps/model/model.service.ts
@@ -0,0 +1,25 @@
+import { AggregatesSetting, Persistence, Model } from './../../model/model-aggregate';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Model } from 'src/app/model/model-aggregate';
+
+@Injectable()
+export class ModelService {
+  private model$ = new BehaviorSubject<Model>(null);
+  getModel$: Observable<Model> = this.model$.asObservable();
+
+  constructor() { }
+
+  addAggregate(aggregates: AggregatesSetting[]): void {
+    const model = this.model$.getValue() || {} as Model;
+    model.aggregatesSettings = aggregates;
+    this.model$.next(model);
+  }
+
+  addPersistence(persistence: Persistence): void {
+    const model = this.model$.getValue() || {} as Model;
+    model.persistence = persistence;
+    this.model$.next(model);
+  }
+
+}
