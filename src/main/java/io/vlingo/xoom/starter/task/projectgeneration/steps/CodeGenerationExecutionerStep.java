99fcc33aa4f7af8b3dddb6cfeae35e9f5ebc1be9
Danilo Ambrosio <danilo.ambrosio@gmail.com>
2020-10-12 11:59:26 -0300

    Code Generation Parameters as single source for project generation

--- src/main/java/io/vlingo/xoom/starter/task/projectgeneration/steps/CodeGenerationExecutionerStep.java
+++ src/main/java/io/vlingo/xoom/starter/task/projectgeneration/steps/CodeGenerationExecutionerStep.java
@@ -4,10 +4,10 @@
 // Mozilla Public License, v. 2.0. If a copy of the MPL
 // was not distributed with this file, You can obtain
 // one at https://mozilla.org/MPL/2.0/.
+
 package io.vlingo.xoom.starter.task.projectgeneration.steps;
 
 import io.vlingo.xoom.codegen.CodeGenerationContext;
-import io.vlingo.xoom.codegen.parameter.CodeGenerationParameters;
 import io.vlingo.xoom.starter.task.TaskExecutionContext;
 import io.vlingo.xoom.starter.task.steps.TaskExecutionStep;
 
@@ -16,19 +16,9 @@ import static io.vlingo.xoom.starter.Configuration.CODE_GENERATION_STEPS;
 public class CodeGenerationExecutionerStep implements TaskExecutionStep {
 
     @Override
-    public void process(final TaskExecutionContext taskExecutionContext) {
-        final CodeGenerationParameters parameters = resolveParameters(taskExecutionContext);
-        final CodeGenerationContext context = CodeGenerationContext.with(parameters);
-        CODE_GENERATION_STEPS.stream()
-                .filter(step -> step.shouldProcess(context))
-                .forEach(step -> step.process(context));
-    }
-
-    private CodeGenerationParameters resolveParameters(final TaskExecutionContext taskExecutionContext) {
-        if(taskExecutionContext.agent().isTerminal()) {
-            return CodeGenerationParametersMapper.of(taskExecutionContext);
-        }
-        return taskExecutionContext.codeGenerationParameters();
+    public void process(final TaskExecutionContext execution) {
+        final CodeGenerationContext generation = CodeGenerationContext.with(execution.codeGenerationParameters());
+        CODE_GENERATION_STEPS.stream().filter(step -> step.shouldProcess(generation)).forEach(step -> step.process(generation));
     }
 
 }
---
+++ src/test/java/io/vlingo/xoom/starter/task/projectgeneration/steps/CodeGenerationParametersLoadStepTest.java
@@ -0,0 +1,65 @@
+// Copyright © 2012-2020 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+package io.vlingo.xoom.starter.task.projectgeneration.steps;
+
+import io.vlingo.xoom.starter.Resource;
+import io.vlingo.xoom.starter.task.TaskExecutionContext;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Paths;
+
+import static io.vlingo.xoom.codegen.parameter.Label.*;
+import static io.vlingo.xoom.starter.task.Agent.TERMINAL;
+import static io.vlingo.xoom.starter.task.Agent.WEB;
+
+public class CodeGenerationParametersLoadStepTest {
+
+    private static final String ROOT_FOLDER = Paths.get(System.getProperty("user.dir"), "dist", "starter").toString();
+
+    @Test
+    public void testThatPropertiesAreLoaded(){
+        final TaskExecutionContext context = TaskExecutionContext.executedFrom(TERMINAL);
+        new CodeGenerationParametersLoadStep().process(context);
+        Assertions.assertFalse(context.codeGenerationParameters().isEmpty());
+        Assertions.assertEquals("1.0", context.codeGenerationParameters().retrieveValue(VERSION));
+        Assertions.assertEquals("com.company", context.codeGenerationParameters().retrieveValue(GROUP_ID));
+        Assertions.assertEquals("xoom-application", context.codeGenerationParameters().retrieveValue(ARTIFACT_ID));
+        Assertions.assertEquals("com.company.business", context.codeGenerationParameters().retrieveValue(PACKAGE));
+        Assertions.assertEquals("true", context.codeGenerationParameters().retrieveValue(USE_ANNOTATIONS));
+        Assertions.assertEquals("MYSQL", context.codeGenerationParameters().retrieveValue(COMMAND_MODEL_DATABASE));
+        Assertions.assertEquals("true", context.codeGenerationParameters().retrieveValue(CQRS));
+        Assertions.assertEquals("HSQLDB", context.codeGenerationParameters().retrieveValue(DATABASE));
+        Assertions.assertEquals("EVENT_BASED", context.codeGenerationParameters().retrieveValue(PROJECTION_TYPE));
+        Assertions.assertEquals("YUGA_BYTE", context.codeGenerationParameters().retrieveValue(QUERY_MODEL_DATABASE));
+        Assertions.assertEquals("true", context.codeGenerationParameters().retrieveValue(AUTO_DISPATCH));
+        Assertions.assertEquals("STATE_STORE", context.codeGenerationParameters().retrieveValue(STORAGE_TYPE));
+        Assertions.assertEquals("DOCKER", context.codeGenerationParameters().retrieveValue(DEPLOYMENT));
+        Assertions.assertEquals("/home/projects", context.codeGenerationParameters().retrieveValue(TARGET_FOLDER));
+        //Assertions.assertEquals(context.codeGenerationParameters().retrieveValue(AGGREGATES), "FirstAggregate;SecondAggregate");
+    }
+
+    @Test
+    public void testThatStepShouldProcess() {
+        Assertions.assertTrue(new CodeGenerationParametersLoadStep().shouldProcess(TaskExecutionContext.executedFrom(TERMINAL)));
+    }
+
+    @Test
+    public void testThatStepShouldNotProcess() {
+        Assertions.assertFalse(new CodeGenerationParametersLoadStep().shouldProcess(TaskExecutionContext.executedFrom(WEB)));
+    }
+
+    @BeforeEach
+    public void setUp() {
+        Resource.clear();
+        Resource.rootIn(ROOT_FOLDER);
+    }
+
+
+}
