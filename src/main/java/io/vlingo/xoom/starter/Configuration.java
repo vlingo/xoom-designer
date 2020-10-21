40498a7132343b14ac6edf80fd410463afa3cfa4
Danilo Ambrosio <danilo.ambrosio@gmail.com>
2020-10-21 20:59:48 -0300

    Code clean up

--- src/main/java/io/vlingo/xoom/starter/Configuration.java
+++ src/main/java/io/vlingo/xoom/starter/Configuration.java
@@ -31,8 +31,6 @@ public class Configuration {
     public static final String USER_INTERFACE_CONFIG_KEY = "ui";
     public static final String PROPERTIES_FILENAME = "vlingo-xoom-starter.properties";
     public static final String HOME_ENVIRONMENT_VARIABLE = "VLINGO_XOOM_STARTER_HOME";
-    public static final ResponseHeader GENERATION_SETTINGS_RESPONSE_HEADER =
-            ResponseHeader.contentType(new ContentMediaType("application", "vnd.generationsettings+json").toString());
 
     public static final Map<Terminal, String> BROWSER_LAUNCH_COMMAND =
             Maps.immutableEnumMap(
