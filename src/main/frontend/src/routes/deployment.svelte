c8f9d83b0022db9c95df51524866a9c56630a63e
Florian-Schoenherr <Florian.Schoenherr.iqb@fh-zwickau.de>
2020-10-21 21:41:11 +0200

    Initialize forms
    Former-commit-id: 0f11584309bb7fc7971d447937a4836249d4c242
    Former-commit-id: 4abef016b4729091f8994c05a5587c19d940d0e2

---
+++ src/main/frontend/src/routes/deployment.svelte
@@ -0,0 +1,27 @@
+<script>
+	import Radio from "svelte-materialify/src/components/Radio";
+import TextField from "svelte-materialify/src/components/TextField";
+	import CardForm from "../components/CardForm.svelte";
+
+	let group = "None";
+	let radios = [
+		{ label: "None" },
+		{ label: "Docker" },
+		{ label: "Kubernetes" },
+	]
+	$: console.log(group);
+</script>
+
+<!-- add newbie tooltips -->
+<CardForm title="Deployment" previous="model" next="generation">
+	{#each radios as radio}
+		<Radio bind:group value={radio.label}>{radio.label}</Radio>
+	{/each}
+	{#if group === "Docker" || group === "Kubernetes"}
+		<TextField placeholder="demo-app">Local Docker Image</TextField>
+	{/if}
+	{#if group === "Kubernetes"}
+		<TextField placeholder="demo-application">Published Docker Image</TextField>
+		<TextField placeholder="demo-application">Kubernetes POD</TextField>
+	{/if}
+</CardForm>
