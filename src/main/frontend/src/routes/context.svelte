845244bddb51fd5b42fce87d41c33bcfd62534fe
Florian-Schoenherr <Florian.Schoenherr.iqb@fh-zwickau.de>
2020-10-27 18:43:27 +0100

    Initalize state and stores
    Former-commit-id: d74ebb4f580a1a5a223da6781c181c2534af0298
    Former-commit-id: 07aee9ea9ee673a64fc9f3f4106df8a7e239a233

--- src/main/frontend/src/routes/context.svelte
+++ src/main/frontend/src/routes/context.svelte
@@ -1,12 +1,23 @@
 <script>
 	import TextField from "svelte-materialify/src/components/TextField";
 	import CardForm from "../components/CardForm.svelte";
+	import { contextSettings } from '../stores';
+	import { notEmpty, validVersion } from '../validators';
+	
+	let groupId = $contextSettings ? $contextSettings.groupId : "";
+	let artifactId = $contextSettings ? $contextSettings.artifactId : "";
+	let artifactVersion = $contextSettings ? $contextSettings.artifactVersion : "";
+	let packageName = $contextSettings ? $contextSettings.packageName : "";
+	let xoomVersion = "1.3.4-SNAPSHOT";
+
+	$: $contextSettings = { groupId, artifactId, artifactVersion, packageName, xoomVersion }
+	$: console.log($contextSettings);
 </script>
 
 <!-- add newbie tooltips -->
 <CardForm title="Context" previous="." next="model">
-	<TextField placeholder="com.example">Group Id</TextField>
-	<TextField placeholder="demo">Artifact Id</TextField>
-	<TextField placeholder="1.0.0">Artifact Version</TextField>
-	<TextField placeholder="com.example.demo">Base Package Name</TextField>
+	<TextField placeholder="com.example" bind:value={groupId} rules={[notEmpty]} validateOnBlur={!groupId}>Group Id</TextField>
+	<TextField placeholder="demo" bind:value={artifactId} rules={[notEmpty]} validateOnBlur={!artifactId}>Artifact Id</TextField>
+	<TextField placeholder="1.0.0" bind:value={artifactVersion} rules={[notEmpty, validVersion]} validateOnBlur={!artifactVersion}>Artifact Version</TextField>
+	<TextField placeholder="com.example.demo" bind:value={packageName} rules={[notEmpty]} validateOnBlur={!packageName}>Base Package Name</TextField>
 </CardForm>
