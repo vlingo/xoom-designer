7b91ee0d09c290aa17624543ee9b3bb76a43f143
Raphael Alves <rsaraphael@gmail.com>
2020-11-05 18:19:33 -0300

    Fix bug with subscribing generation settings values when click on previous button

--- user-interface/src/app/steps/generation/generation.component.ts
+++ user-interface/src/app/steps/generation/generation.component.ts
@@ -1,12 +1,14 @@
 import { ToastrService } from 'ngx-toastr';
 import { GenerationSettingsService } from './../../service/generation-settings.service';
 import { SettingsStepService } from '../../service/settings-step.service';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { StepComponent } from '../step.component';
 import { StepCompletion } from 'src/app/model/step-completion';
 import { NavigationDirection } from 'src/app/model/navigation-direction';
 import { Step } from 'src/app/model/step';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { tap, catchError, finalize, take } from 'rxjs/operators';
+import { EMPTY } from 'rxjs';
 
 @Component({
   selector: 'app-generation',
@@ -36,14 +38,17 @@ export class GenerationComponent extends StepComponent {
 
   generate() {
     const value = this.generationForm.value;
-    this.settingsStepService.getSettings$.subscribe(settings => {
+    this.settingsStepService.getSettings$.pipe(take(1), tap(settings => {
       settings.projectDirectory = value.projectDirectory;
       settings.useAnnotations = value.useAnnotations;
       settings.useAutoDispatch = value.useAutoDispatch;
-      this.generationSettingsService.generate(settings).subscribe(response => {
+      this.generationSettingsService.generate(settings).pipe(tap(() => {
         this.toastrService.success('Code generated. Please check folder ' + value.projectDirectory);
+      }), () => {
+        this.toastrService.error('Code not generated. Please check the values and try again.');
+        return EMPTY;
       });
-    });
+    })).subscribe();
     this.stepCompletion.emit(new StepCompletion(
       Step.GENERATION,
       this.generationForm.valid,
@@ -53,7 +58,7 @@ export class GenerationComponent extends StepComponent {
 
   onAnnotationsClick($event) {
     this.generationSettings.useAnnotations = $event.target.checked;
-    if(!this.generationSettings.useAnnotations) {
+    if (!this.generationSettings.useAnnotations) {
       this.generationSettings.useAutoDispatch = false;
     }
   }
