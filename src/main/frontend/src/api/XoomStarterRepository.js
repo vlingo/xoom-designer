9d26dd14f62a31c18bca25424e66e3a64d79d984
Florian-Schoenherr <Florian.Schoenherr.iqb@fh-zwickau.de>
2020-11-08 16:02:06 +0100

    Call API on generate

---
+++ src/main/frontend/src/api/XoomStarterRepository.js
@@ -0,0 +1,28 @@
+import Repository from './Repository'
+
+const resources = Object.freeze({
+	generation: '/generation-settings',
+})
+
+function ensure(response, status) {
+	if (response.status !== status) {
+		throw Error(`HTTP ${response.status}: ${response.statusText} ().`);
+	}
+	return response;
+}
+  
+function ensureOk(response) {
+	return ensure(response, 200);
+}
+  
+async function repoPost(path, body) {
+	return Repository.post(path, body)
+	.then(ensureOk)
+	.then(res => res.json());
+}
+  
+export default {
+	postGenerationSettings(generationSettingsData) {
+		return repoPost(resources.generation, { generationSettingsData });
+	},
+}
