6152a74dc64d3bb9eada0801d3c6090b69fb5c08
Danilo Ambrosio <danilo.ambrosio@gmail.com>
2020-11-09 18:40:54 -0300

    Fix test

--- src/test/java/io/vlingo/xoom/starter/task/projectgeneration/steps/ArchetypeCommandResolverStepTest.java
+++ src/test/java/io/vlingo/xoom/starter/task/projectgeneration/steps/ArchetypeCommandResolverStepTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.nio.file.Paths;
 
 import static io.vlingo.xoom.codegen.parameter.Label.*;
@@ -30,8 +31,9 @@ public class ArchetypeCommandResolverStepTest {
             Paths.get(DEFAULT_ROOT_FOLDER, "resources", "archetypes").toString();
 
     private static final String EXPECTED_ARCHETYPE_COMMAND_ON_WINDOWS =
-                    "D: && cd " + WINDOWS_ARCHETYPE_PATH + " && mvnw.cmd -f kubernetes-archetype\\pom.xml clean install " +
-                    "&& mvnw.cmd archetype:generate -B -DarchetypeCatalog=internal -DarchetypeGroupId=io.vlingo " +
+                    "D: && cd " + WINDOWS_ARCHETYPE_PATH + " && mvnw.cmd -f kubernetes-archetype" + File.separator +
+                    "pom.xml clean install " + "&& mvnw.cmd archetype:generate -B -DarchetypeCatalog=internal " +
+                    "-DarchetypeGroupId=io.vlingo " +
                     "-DarchetypeArtifactId=vlingo-xoom-kubernetes-archetype " +
                     "-DarchetypeVersion=1.0 -Dversion=1.0 -DgroupId=io.vlingo " +
                     "-DartifactId=starter-example -DmainClass=io.vlingo.starterexample.infrastructure.Bootstrap " +
@@ -41,8 +43,9 @@ public class ArchetypeCommandResolverStepTest {
                     "&& MOVE /Y starter-example E:\\projects";
 
     private static final String EXPECTED_ARCHETYPE_COMMAND =
-                    "cd " + DEFAULT_ARCHETYPE_PATH +  " && mvnw -f .\\kubernetes-archetype\\pom.xml clean install " +
-                    "&& mvnw archetype:generate -B -DarchetypeCatalog=internal -DarchetypeGroupId=io.vlingo " +
+                    "cd " + DEFAULT_ARCHETYPE_PATH +  " && mvnw -f ." + File.separator +  "kubernetes-archetype"
+                    + File.separator + "pom.xml clean install && mvnw archetype:generate -B " +
+                    "-DarchetypeCatalog=internal -DarchetypeGroupId=io.vlingo " +
                     "-DarchetypeArtifactId=vlingo-xoom-kubernetes-archetype " +
                     "-DarchetypeVersion=1.0 -Dversion=1.0 -DgroupId=io.vlingo " +
                     "-DartifactId=starter-example -DmainClass=io.vlingo.starterexample.infrastructure.Bootstrap " +
