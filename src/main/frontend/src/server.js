fbd2b60411160ac3820276d1002d6a1311d23edb
Florian-Schoenherr <Florian.Schoenherr.iqb@fh-zwickau.de>
2020-11-10 18:37:16 +0100

    Proxy

--- src/main/frontend/src/server.js
+++ src/main/frontend/src/server.js
@@ -2,11 +2,13 @@ import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
+	.use('/api', createProxyMiddleware({ target: "http://localhost:19090", changeOrigin: true }))
 	.use(
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
