42e24ec9e067642c9546983d21ecdde5c1fba630
Florian-Schoenherr <Florian.Schoenherr.iqb@fh-zwickau.de>
2020-11-11 16:36:57 +0100

    Refactor, bind useFactory to POST
    Former-commit-id: 26a197467198983b7606f4c8d852821fdafa872f
    Former-commit-id: 5ebfed0f3f4ed5b614756ce4b74852e4c10207b6

--- src/main/frontend/src/routes/aggregates.svelte
+++ src/main/frontend/src/routes/aggregates.svelte
@@ -11,15 +11,11 @@
 	import { aggregateSettings } from "../stores";
 	import { notEmpty } from "../validators";
 	import Dialog from "svelte-materialify/src/components/Dialog";
-
-	function formatArrayForSelect(array) {
-		return array.map(
-			element => ({ name: element, value: element })
-		)
-	}
+	import Method from "../components/aggregates/Method.svelte";
+	import Route from "../components/aggregates/Route.svelte";
+	import { formatArrayForSelect } from "../utils";
 
 	const stateFieldsTypes =  formatArrayForSelect(['int', 'double', 'String', 'float', 'short', 'byte', 'boolean', 'long', 'char']);
-	const httpMethods = formatArrayForSelect(['POST', 'PUT', 'DELETE', 'PATCH', 'GET', 'HEAD', 'OPTIONS']);
 	let dialogActive = false;
 
 	let aggregateName = "";
@@ -34,9 +30,7 @@
 	const addEvent = () => events = events.concat({ name: "", fields: [] });
 	const deleteEvent = (index) => { events.splice(index, 1); events = events; }
 	const addMethod = () => methods = methods.concat({ name: "", useFactory: false, parameters: [], event: "" });
-	const deleteMethod = (index) => { methods.splice(index, 1); methods = methods; }
 	const addRoute = () => routes = routes.concat({ path: "", httpMethod: "", aggregateMethod: "", requireEntityLoad: false });
-	const deleteRoute = (index) => { routes.splice(index, 1); routes = routes; }
 	
 	$: aggregateSetting = { aggregateName, stateFields, events, methods, api: { rootPath, routes } }
 
@@ -45,7 +39,8 @@
 		dialogActive = !dialogActive;
 	}
 
-	$: console.log($aggregateSettings, dialogActive);
+	$: console.log(aggregateSetting);
+	$: console.log($aggregateSettings);
 </script>
 
 <!-- add newbie tooltips -->
@@ -54,81 +49,67 @@
 </CardForm>
 
 <Dialog bind:active={dialogActive} width={1000} class="pa-4">
+	<h4 style="text-align: center;">New Aggregate</h4>
 	<TextField bind:value={aggregateName} rules={[notEmpty]} validateOnBlur={!aggregateName}>Aggregate Name</TextField>
-	<Divider class="ma-2" />
+	<!-- <Divider class="ma-2" /> -->
 
 	<h5>State Fields:</h5>
 	{#each stateFields as stateField, i}
 		<span class="d-flex">
 			<TextField class="ma-2" bind:value={stateField.name}>Name</TextField>
 			<Select class="ma-2" items={stateFieldsTypes} bind:value={stateField.type}>Type</Select>
-			<Button on:click={() => deleteStateField(i)} icon class="ma-2 red-text">
+			<Button title="Delete State Field" on:click={() => deleteStateField(i)} icon class="ma-2 red-text">
 				<Icon path={mdiDelete}/>
 			</Button>
 		</span>
 	{/each}
 	<div class="d-flex justify-center">
-		<Button fab on:click={addStateField} class="ma-2">
+		<Button title="Add State Field" fab on:click={addStateField} class="ma-2">
 			<Icon path={mdiPlusThick}/>
 		</Button>
 	</div>
-	<Divider class="ma-2" />
+	<!-- <Divider class="ma-2" /> -->
 
 	<h5>Events:</h5>
 	{#each events as event, i}
 		<span class="d-flex">
 			<TextField class="ma-2" bind:value={event.name}>Name</TextField>
 			<Select multiple class="ma-2" items={formatArrayForSelect(stateFields.map(f => f.name))} bind:value={event.fields}>Fields</Select>
-			<Button on:click={() => deleteEvent(i)} icon class="ma-2 red-text">
+			<Button title="Delete Event" on:click={() => deleteEvent(i)} icon class="ma-2 red-text">
 				<Icon path={mdiDelete}/>
 			</Button>
 		</span>
 	{/each}
 	<div class="d-flex justify-center">
-		<Button fab on:click={addEvent} class="ma-2">
+		<Button title="Add Event" fab on:click={addEvent} class="ma-2">
 			<Icon path={mdiPlusThick}/>
 		</Button>
 	</div>
-	<Divider class="ma-2" />
+	<!-- <Divider class="ma-2" /> -->
 
 	<h5>Methods:</h5>
-	{#each methods as method, i}
-		<span class="d-flex">
-			<TextField class="ma-2" bind:value={method.name}>Name</TextField>
-			<Switch bind:checked={method.useFactory}>Use Factory</Switch>
-			<Select multiple class="ma-2" items={formatArrayForSelect(stateFields.map(f => f.name))} bind:value={method.parameters}>Parameters</Select>
-			<Select class="ma-2" items={formatArrayForSelect(events.map(e => e.name))} bind:value={method.event}>Event</Select>
-			<Button on:click={() => deleteMethod(i)} icon class="ma-2 red-text">
-				<Icon path={mdiDelete}/>
-			</Button>
-		</span>
+	{#each methods as method, id}
+		<Method bind:method {id} stateFields={formatArrayForSelect(stateFields.map(f => f.name))} events={formatArrayForSelect(events.map(e => e.name))}/>
 	{/each}
 	<div class="d-flex justify-center">
-		<Button fab on:click={addMethod} class="ma-2">
+		<Button title="Add Method" fab on:click={addMethod} class="ma-2">
 			<Icon path={mdiPlusThick}/>
 		</Button>
 	</div>
-	<Divider class="ma-2" />
+	<!-- <Divider class="ma-2" /> -->
 
 	<h5>API:</h5>
 	<TextField class="ma-2" bind:value={rootPath}>Root Path</TextField>
-	{#each routes as route, i}
-		<span class="d-flex align-center">
-			<PathField bind:path={route.path} bind:requireEntityLoad={route.requireEntityLoad}/>
-			<Select class="ma-2" items={httpMethods} bind:value={route.httpMethod}>Http Request Method</Select>
-			<Select class="ma-2" items={formatArrayForSelect(methods.map(m => m.name))} bind:value={route.aggregateMethod}>Aggregate Method</Select>
-			<Switch class="ma-2" bind:checked={route.requireEntityLoad}>Require Entity Load</Switch>
-			<Button on:click={() => deleteRoute(i)} icon class="ma-2 red-text">
-				<Icon path={mdiDelete}/>
-			</Button>
-		</span>
+
+	{#each routes as { path, httpMethod, aggregateMethod, requireEntityLoad } , id}
+		<Route bind:path bind:httpMethod bind:aggregateMethod bind:requireEntityLoad {id} bind:methods bind:routes/>
 	{/each}
 	<div class="d-flex justify-center">
-		<Button fab on:click={addRoute} class="ma-2">
+		<Button title="Add Route" fab on:click={addRoute} class="ma-2">
 			<Icon path={mdiPlusThick}/>
 		</Button>
 	</div>
-	<Divider class="ma-2" />
+	<!-- <Divider class="ma-2" /> -->
 	<Button on:click={addAggregate}>Add</Button>
 	<Button on:click={() => dialogActive = !dialogActive}>Cancel</Button>
 </Dialog>
