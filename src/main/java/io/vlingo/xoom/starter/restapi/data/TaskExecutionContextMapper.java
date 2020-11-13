578e88e279580ec04082a5aa62e63374cc75caec
Danilo Ambrosio <danilo.ambrosio@gmail.com>
2020-11-13 11:37:37 -0300

    Obsolete gen parameter
    Former-commit-id: 13bcd5e1ec4a375b5da96ec6b7ca726b560fa060
    Former-commit-id: b66d947880af03409709afa1719a030e61930fff

--- src/main/java/io/vlingo/xoom/starter/restapi/data/TaskExecutionContextMapper.java
+++ src/main/java/io/vlingo/xoom/starter/restapi/data/TaskExecutionContextMapper.java
@@ -117,7 +117,7 @@ public class TaskExecutionContextMapper {
                 .add(ARTIFACT_ID, data.context.artifactId)
                 .add(VERSION, data.context.artifactVersion)
                 .add(PACKAGE, data.context.packageName)
-                .add(XOOM_SERVER_VERSION, Configuration.resolveDefaultXoomVersion())
+                .add(XOOM_VERSION, Configuration.resolveDefaultXoomVersion())
                 .add(DEPLOYMENT, data.deployment.type)
                 .add(DOCKER_IMAGE, data.deployment.dockerImage)
                 .add(KUBERNETES_IMAGE, data.deployment.kubernetesImage)
