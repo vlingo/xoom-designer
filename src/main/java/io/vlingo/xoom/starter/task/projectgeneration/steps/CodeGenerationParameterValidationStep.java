f68e0085cbdaa684de91ee426e2df559b7cc0cb3
Florian-Schoenherr <Florian.Schoenherr.iqb@fh-zwickau.de>
2020-11-17 17:17:06 +0100

    Aggregate validation
    Signed-off-by: Florian-Schoenherr <Florian.Schoenherr.iqb@fh-zwickau.de>

--- src/main/java/io/vlingo/xoom/starter/task/projectgeneration/steps/CodeGenerationParameterValidationStep.java
+++ src/main/java/io/vlingo/xoom/starter/task/projectgeneration/steps/CodeGenerationParameterValidationStep.java
@@ -11,33 +11,42 @@ import io.vlingo.xoom.codegen.parameter.Label;
 import io.vlingo.xoom.codegen.template.projections.ProjectionType;
 import io.vlingo.xoom.codegen.template.storage.StorageType;
 
-import static io.vlingo.xoom.codegen.parameter.Label.GROUP_ID;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
 import io.vlingo.xoom.starter.task.TaskExecutionContext;
+import io.vlingo.xoom.starter.task.projectgeneration.SupportedTypes;
 import io.vlingo.xoom.starter.task.steps.TaskExecutionStep;
 
 public class CodeGenerationParameterValidationStep implements TaskExecutionStep {
 
+    //some of these are not complete
     //first word is lowercase letters, after that we match first letter a-zA-Z_, then a-zA-z_ and digits, then repeat last two steps
     private static String PACKAGE_PATTERN = "^[a-z]+(.[a-zA-Z_]([a-zA-Z_$#\\d])*)+$";
     private static String ARTIFACT_PATTERN = "^[a-z-]+$";
     private static String VERSION_PATTERN = "^\\d+.\\d+.\\d+$";
+    private static String CLASSNAME_PATTERN = "^[A-Z]+[A-Za-z]*$";
+    private static String FIELDNAME_PATTERN = "^[a-zA-Z_$][a-zA-Z_$0-9]*$";
+    private static String ROUTE_PATTERN = "^[a-zA-Z_$/?]+$";
     private static CodeGenerationParameters parameters;
 
     @Override
     public void process(final TaskExecutionContext context) {
         parameters = context.codeGenerationParameters();
         List<String> errorStrings = new ArrayList<>();
-        if(!retrieve(GROUP_ID).matches(PACKAGE_PATTERN)) errorStrings.add("GroupID must follow package pattern");
+        if(!retrieve(Label.GROUP_ID).matches(PACKAGE_PATTERN)) errorStrings.add("GroupID must follow package pattern");
         if(!retrieve(Label.ARTIFACT_ID).matches(ARTIFACT_PATTERN)) errorStrings.add("ArtifactID must consist of lowercase letters and hyphens");
         if(!retrieve(Label.VERSION).matches(VERSION_PATTERN)) errorStrings.add("Version must be a semantic version");
         if(!retrieve(Label.PACKAGE).matches(PACKAGE_PATTERN)) errorStrings.add("Package must follow package pattern");
         if(!isXoomVersionValid()) errorStrings.add("Xoom version must be either 1.4.1-SNAPSHOT or 1.4.0");
-        // retrieve(Label.AGGREGATE)
+
+        if(!areAggregateNamesValid()) errorStrings.add("Aggregate names must follow classname pattern");
+        if(!areStateFieldsValid()) errorStrings.add("State Fields must follow fieldname pattern");
+        if(!areDomainEventsValid()) errorStrings.add("Domain Events must follow classname pattern");
+        if(!areMethodsValid()) errorStrings.add("Methods are not valid");
+        if(!areRestResourcesValid()) errorStrings.add("Rest Resources are not valid");
+
         if(!isStorageTypeValid()) errorStrings.add("StorageType is not valid");
         if(!isProjectionValid(retrieve(Label.PROJECTION_TYPE), retrieve(Label.STORAGE_TYPE))) errorStrings.add("ProjectionType is not valid");
         if(!isDatabaseValid()) errorStrings.add("Database is not valid");
@@ -49,9 +58,67 @@ public class CodeGenerationParameterValidationStep implements TaskExecutionStep 
             throw new IllegalArgumentException(errorMessage);
         }
     }
+
     private String retrieve(Label label) {
         return parameters.retrieveValue(label);
     }
+
+    private boolean areAggregateNamesValid() {
+        return parameters.retrieveAll(Label.AGGREGATE).allMatch(aggregateName -> aggregateName.value.matches(CLASSNAME_PATTERN));
+    }
+
+    private boolean areStateFieldsValid() {
+        return parameters.retrieveAll(Label.AGGREGATE).map(
+            aggregate -> aggregate.retrieveAll(Label.STATE_FIELD).allMatch(stateField -> stateField.value.matches(FIELDNAME_PATTERN) &&
+                stateField.retrieveAll(Label.FIELD_TYPE).allMatch(type -> 
+                    Stream.of(
+                        SupportedTypes.INTEGER.name(),
+                        SupportedTypes.LONG.name(),
+                        SupportedTypes.FLOAT.name(),
+                        SupportedTypes.DOUBLE.name(),
+                        SupportedTypes.STRING.name(),
+                        SupportedTypes.BOOLEAN.name()
+                    ).anyMatch(type.value::equals)
+                )
+            )
+        ).allMatch(bool -> bool==true);
+    }
+
+    private boolean areDomainEventsValid() {
+        return parameters.retrieveAll(Label.AGGREGATE).map(
+            aggregate -> aggregate.retrieveAll(Label.DOMAIN_EVENT).allMatch(event -> event.value.matches(CLASSNAME_PATTERN))
+        ).allMatch(bool -> bool==true);
+    }
+
+    private boolean areMethodsValid() {
+        return parameters.retrieveAll(Label.AGGREGATE).map(
+            aggregate -> aggregate.relatedParameterValueOf(Label.AGGREGATE_METHOD).matches(FIELDNAME_PATTERN)
+        ).allMatch(bool -> bool==true);
+    }
+
+    private boolean areRestResourcesValid() {
+        return parameters.retrieveAll(Label.AGGREGATE)
+        .map(aggregate ->
+            aggregate.retrieveAll(Label.REST_RESOURCES).allMatch(rest ->
+                rest.value.startsWith("/") &&
+                rest.relatedParameterValueOf(Label.ROUTE_PATH).matches(ROUTE_PATTERN) &&
+                aggregate.retrieveAll(Label.AGGREGATE_METHOD).map(
+                    method -> method.value.equals(rest.relatedParameterValueOf(Label.ROUTE_SIGNATURE))
+                ).allMatch(bool -> bool==true) &&
+                Stream.of(
+                    "POST",
+                    "PUT",
+                    "DELETE",
+                    "PATCH",
+                    "GET",
+                    "HEAD",
+                    "OPTIONS"
+                    ).anyMatch(rest.relatedParameterValueOf(Label.ROUTE_METHOD)::equals)
+            )
+        ).allMatch(bool -> bool==true);
+    }
+
+    // parameters.retrieve(Label.AGGREGATE).relatedParameterOf(Label.STATE_FIELD)
 
     private boolean isXoomVersionValid() {
         return retrieve(Label.XOOM_VERSION).equals("1.4.1-SNAPSHOT") && !retrieve(Label.XOOM_VERSION).equals("1.4.0");
