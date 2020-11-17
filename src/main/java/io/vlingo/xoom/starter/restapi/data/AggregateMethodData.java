30759761cc8fa6c14ed78c30e1ac4b4b933c956e
Florian-Schoenherr <Florian.Schoenherr.iqb@fh-zwickau.de>
2020-11-17 14:50:26 +0100

    Better error messages
    Signed-off-by: Florian-Schoenherr <Florian.Schoenherr.iqb@fh-zwickau.de>
    
    Former-commit-id: ab2eef3ac1cf775355e14f21fcb57e043a10fbf6
    Former-commit-id: 00fdd5c40426e0237fafc9e2c0a5aae616865fe2

--- src/main/java/io/vlingo/xoom/starter/restapi/data/AggregateMethodData.java
+++ src/main/java/io/vlingo/xoom/starter/restapi/data/AggregateMethodData.java
@@ -26,10 +26,10 @@ public class AggregateMethodData {
     }
 
     public List<String> validate(List<String> errorStrings) {
-        if(name==null) errorStrings.add("name is null");
-        if(parameters==null) errorStrings.add("parameters is null");
-        if(factory==null) errorStrings.add("factory is null");
-        if(event==null) errorStrings.add("event is null");
+        if(name==null) errorStrings.add("AggregateMethodData.name is null");
+        if(parameters==null) errorStrings.add("AggregateMethodData.parameters is null");
+        if(factory==null) errorStrings.add("AggregateMethodData.factory is null");
+        if(event==null) errorStrings.add("AggregateMethodData.event is null");
         return errorStrings;
     }
 }
