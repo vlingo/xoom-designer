8b29d79e70e563759efec8b605a043ef40eee537
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-01-28 00:12:07 +0300

    deployment store is created
    Signed-off-by: abdullahcalisir12 <abdullahcalisir12@gmail.com>
    Signed-off-by: hurelhuyag <hurelhuyag@gmail.com>

---
+++ src/main/frontend/src/stores/utils.js
@@ -0,0 +1,29 @@
+import { writable } from 'svelte/store';
+
+export function createLocalStore(key, initialValue) {
+	const localValue = process.browser ? localStorage.getItem(key) : initialValue;
+	const { subscribe, set } = writable(localValue);
+
+	return {
+		subscribe,
+		set: (value) => {
+			if (process.browser) {
+				localStorage.setItem(key, value);
+			}
+			set(value)
+		},
+	};
+}
+
+export function isMobileStore() {
+	const { subscribe, set } = writable(false);
+	return {
+		subscribe,
+		set,
+		check: () => {
+			import('svelte-materialify/src/utils/breakpoints').then(({ default: breakpoints }) => {
+				set(window.matchMedia(breakpoints['md-and-down']).matches);
+			});
+		},
+	};
+};
