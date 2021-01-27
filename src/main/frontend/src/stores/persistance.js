334d59a4218cabb5958858775d30230f8bc2f251
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-01-28 00:29:11 +0300

    persistance readable storage is added
    Signed-off-by: abdullahcalisir12 <abdullahcalisir12@gmail.com>

---
+++ src/main/frontend/src/stores/persistance.js
@@ -0,0 +1,26 @@
+import { readable } from 'svelte/store';
+
+export const storageTypes = readable(null, function start(set) {
+	set([
+    {name: "State Store", value: "STATE_STORE"},
+    {name: "Journal", value: "JOURNAL"}
+  ]);
+});
+
+export const projectionsTypes = readable(null, function start(set) {
+	set([
+		{name: 'Not Applicable', value: 'NONE'},
+		{name: 'Event Based', value: 'EVENT_BASED'},
+		{name: 'Operation Based', value: 'OPERATION_BASED'}
+  ]);
+});
+
+export const databaseTypes = readable(null, function start(set) {
+	set([
+    {name: "In Memory", value: "IN_MEMORY"},
+    {name: "Postgres", value: "POSTGRES"},
+    {name: "HSQLDB", value: "HSQLDB"},
+    {name: "MySQL", value: "MYSQL"},
+    {name: "YugaByte", value: "YUGA_BYTE"}
+  ]);
+});
