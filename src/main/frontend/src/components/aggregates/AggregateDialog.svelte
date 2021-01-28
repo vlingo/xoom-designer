7073572a2441d1efe0440e023d9c4961a7c03385
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-01-29 01:26:21 +0300

    AggregateDialog is updated using newly created components
    Signed-off-by: abdullahcalisir12 <abdullahcalisir12@gmail.com>

--- src/main/frontend/src/components/aggregates/AggregateDialog.svelte
+++ src/main/frontend/src/components/aggregates/AggregateDialog.svelte
@@ -1,20 +1,20 @@
 <script>
 	import {
 		Button,
-		Select,
 		TextField,
 		Dialog,
 		CardActions
 	} from 'svelte-materialify/src';
-	import Method from "./Method.svelte";
-	import Route from "./Route.svelte";
 	import { aggregateSettings, currentAggregate, setLocalStorage } from "../../stores";
-	import { classNameRule, identifierRule, requireRule, routeRule, schemaGroupRule, schemaRule } from "../../validators";
-	import { formatArrayForSelect } from "../../utils";
-	import DeleteButton from "./DeleteButton.svelte";
-	import CreateButton from "./CreateButton.svelte";
+	import { classNameRule, identifierRule, requireRule, routeRule } from "../../validators";
 
-	const stateFieldsTypes =  formatArrayForSelect(['int', 'double', 'String', 'float', 'short', 'byte', 'boolean', 'long', 'char']);
+	import StateFields from './StateFields.svelte';
+	import Events from './Events.svelte';
+	import Methods from './Methods.svelte';
+	import Routes from './Routes.svelte';
+	import ProducerExchange from './ProducerExchange.svelte';
+	import ConsumerExchange from './ConsumerExchange.svelte';
+
 	export let dialogActive;
 	export let editMode;
 
@@ -32,25 +32,6 @@
 	let routes = [];
 	let outgoingEvents = [];
 	let receivers = [];
-
-	const addStateField = () => stateFields = stateFields.concat({ name: "", type: "" });
-	const deleteStateField = (index) => { stateFields.splice(index, 1); stateFields = stateFields; }
-	const addEvent = () => events = events.concat({ name: "", fields: ["id"] });
-	const deleteEvent = (index) => { events.splice(index, 1); events = events; }
-	const addMethod = () => methods = methods.concat({ name: "", useFactory: false, parameters: [], event: "" });
-	const addRoute = () => routes = routes.concat({ path: "", httpMethod: "GET", aggregateMethod: "", requireEntityLoad: false });
-	const addReceiver = () => {
-		if(receivers.length > 0) {
-			const lastReceiver = receivers[receivers.length-1];
-			const schemaPrefix = lastReceiver.schema.split(":").splice(0, 3).join(":");
-			const schemaPlaceholder = schemaPrefix + ":[Enter the schema name]:0.0.1";
-			receivers = receivers.concat({ aggregateMethod: "", schema: schemaPlaceholder})
-		} else {
-			receivers = receivers.concat({ aggregateMethod: "", schema: "" })
-		}
-	}
-	const deleteReceiver = (index) => { receivers.splice(index, 1); receivers = receivers; }
-
 
 	const add = () => {
 		if(requireRule(aggregateName)) return;
@@ -92,38 +73,6 @@
 		return currentId == 0 || (schemaGroup == undefined && schemaGroup.length == 0);
 	}
 
-		//The items selected in the "event-fields" combo, which are binded in the "events.fields" array, 
-		//have to follow the same order of "stateFields" items.
-		//the items can be matched by name (eg: "eventField.name == stateField.name").
-
-		/* There was no on:change function emitted from svelte-materialify Select component,
-		*  so function would not be called at any moment, instead I used this piece of code to achive what you described in your comment
-		*/
-
-	$: {
-		events = events.map((event) => {
-			return {
-				...event,
-				fields: stateFields.reduce((acc, cur) => {
-					if (event.fields && event.fields.includes(cur.name)) acc.push(cur.name);
-					return acc;
-				}, [])
-			};
-		})
-	}
-		
-	$: {
-		methods = methods.map((method) => {
-			return {
-				...method,
-				parameters: stateFields.reduce((acc, cur) => {
-					if (method.parameters && method.parameters.includes(cur.name)) acc.push(cur.name);
-					return acc;
-				}, [])
-			};
-		})
-	}
-
 	$: changedCurrent(currentId);
 	function changedCurrent(id) {
 		console.log(id, $aggregateSettings[id]);
@@ -163,69 +112,17 @@
 	</h4>
 	<TextField bind:value={aggregateName} rules={[requireRule, classNameRule]} validateOnBlur={!aggregateName}>Aggregate Name</TextField>
 	<!-- <Divider class="ma-2" /> -->
-
-	<h5>State Fields:</h5>
-	{#each stateFields as stateField, i}
-		<span class="d-flex">
-		<div style="max-width: 100%">
-			<TextField disabled={i<1} class="ma-2" bind:value={stateField.name} rules={[requireRule, identifierRule]}>Name</TextField>
-		</div>
-		<div style="max-width: 100%">
-			<Select mandatory disabled={i<1} class="ma-2" items={stateFieldsTypes} bind:value={stateField.type}>Type</Select>
-		</div>
-		{#if stateField.name !== 'id'}
-			<DeleteButton title="Delete State Field" on:click={() => deleteStateField(i)}/>
-		{/if}
-		</span>
-	{/each}
-	<CreateButton title="Add State Field" on:click={addStateField}/>
+	<StateFields bind:stateFields />
 	<!-- <Divider class="ma-2" /> -->
-
-	<h5>Events:</h5>
-	{#each events as event, i}
-		<span class="d-flex">
-			<TextField class="ma-2" bind:value={event.name} rules={[requireRule, classNameRule]} validateOnBlur={!event.name}>Name</TextField>
-			<Select id="event-fields" mandatory disabled={!stateFields.length} multiple class="ma-2" items={formatArrayForSelect(stateFields.map(f => f.name))} bind:value={event.fields}>Fields</Select>
-			<DeleteButton title="Delete Event" on:click={() => deleteEvent(i)}/>
-		</span>
-	{/each}
-	<CreateButton title="Add Event" on:click={addEvent}/>
+	<Events bind:events  bind:stateFields />
 	<!-- <Divider class="ma-2" /> -->
-
-	<h5>Methods:</h5>
-	{#each methods as method, id}
-		<Method bind:method {id} stateFields={formatArrayForSelect(stateFields.map(f => f.name))} events={formatArrayForSelect(events.map(e => e.name))} bind:methods/>
-	{/each}
-	<CreateButton title="Add Method" on:click={addMethod}/>
+	<Methods bind:methods bind:stateFields bind:events />
 	<!-- <Divider class="ma-2" /> -->
-
-	<h5>API:</h5>
-	<TextField class="ma-2" bind:value={rootPath} rules={[requireRule, routeRule]} validateOnBlur={!rootPath}>Root Path</TextField>
-
-	{#each routes as { path, httpMethod, aggregateMethod, requireEntityLoad } , id}
-		<Route bind:path bind:httpMethod bind:aggregateMethod bind:requireEntityLoad {id} bind:methods bind:routes/>
-	{/each}
-	<CreateButton title="Add Route" on:click={addRoute}/>
+	<Routes bind:routes bind:methods bind:rootPath />
 	<!-- <Divider class="ma-2" /> -->
-	
-	<h5>Producer Exchange:</h5>
-	<span class="d-flex">
-		<TextField class="ma-2" bind:value={producerExchangeName}>Exchange Name</TextField>
-		<TextField class="ma-2" bind:value={schemaGroup} rules={[schemaGroupRule]} validateOnBlur={!schemaGroup} disabled={disableSchemaGroup}>Organization : Unit : Context</TextField>
-	</span>
-	<Select mandatory disabled={!events.length} multiple class="ma-2" items={formatArrayForSelect(events.map(e => e.name))} bind:value={outgoingEvents}>Domain Event</Select>
+	<ProducerExchange bind:events bind:producerExchangeName bind:outgoingEvents bind:schemaGroup bind:disableSchemaGroup  />
 
-	<h5>Consumer Exchange:</h5>
-	<TextField class="ma-2" bind:value={consumerExchangeName}>Exchange Name</TextField>
-
-	{#each receivers as receiver, i}
-	<span class="d-flex">
-		<TextField class="ma-2" bind:value={receiver.schema} rules={[schemaRule]} validateOnBlur={!(receiver.schema)}>Schema Reference</TextField>
-		<Select mandatory class="ma-2" items={formatArrayForSelect(methods.map(m => m.name))} bind:value={receiver.aggregateMethod}>Aggregate Method</Select>
-		<DeleteButton title="Delete Schema" on:click={() => deleteReceiver(i)}/>
-	</span>
-	{/each}
-	<CreateButton title="Add Schema" on:click={addReceiver}/>
+	<ConsumerExchange bind:consumerExchangeName bind:receivers bind:methods />
 
 	<CardActions>
 		{#if editMode}
