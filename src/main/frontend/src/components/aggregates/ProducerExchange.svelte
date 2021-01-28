91b8acfa82a97a32c2808b9a3ae3a9c89b542096
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-01-29 01:25:53 +0300

    ProducerComponent is created
    Signed-off-by: abdullahcalisir12 <abdullahcalisir12@gmail.com>
    Signed-off-by: hurelhuyag <hurelhuyag@gmail.com>

---
+++ src/main/frontend/src/components/aggregates/ProducerExchange.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { Select, TextField } from 'svelte-materialify/src';
+	import { schemaGroupRule } from "../../validators";
+  import { formatArrayForSelect } from "../../utils";
+
+  export let events;
+  export let producerExchangeName;
+  export let outgoingEvents;
+  export let schemaGroup;
+  export let disableSchemaGroup;
+</script>
+
+<h5>Producer Exchange:</h5>
+<span class="d-flex">
+  <TextField class="ma-2" bind:value={producerExchangeName}>Exchange Name</TextField>
+  <TextField class="ma-2" bind:value={schemaGroup} rules={[schemaGroupRule]} validateOnBlur={!schemaGroup} disabled={disableSchemaGroup}>Organization : Unit : Context</TextField>
+</span>
+<Select mandatory disabled={!events.length} multiple class="ma-2" items={formatArrayForSelect(events.map(e => e.name))} bind:value={outgoingEvents}>Domain Event</Select>
