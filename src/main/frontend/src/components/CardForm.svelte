3764046f7928f8c81d30d9d13d17bd292a620d3d
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-01-30 02:21:23 +0300

    cardForm design improvements
    Signed-off-by: abdullahcalisir12 <abdullahcalisir12@gmail.com>

--- src/main/frontend/src/components/CardForm.svelte
+++ src/main/frontend/src/components/CardForm.svelte
@@ -1,30 +1,36 @@
 <script>
-	import { Button, Card, CardActions, CardText, CardTitle } from 'svelte-materialify/src';
-	
+	import { Button, Card, CardActions, CardText, CardTitle, Icon } from 'svelte-materialify/src';
+	import { mdiArrowLeft, mdiArrowRight } from "@mdi/js";
 	export let title = "";
 	export let previous = "";
 	export let next = "";
 	
 </script>
-
-<Card style="display: flex;" class="justify-center align-center flex-column">
-	<CardTitle>{title}</CardTitle>
-	<CardText>
+<Card
+	style="display: flex; box-shadow: 0 0 64px 16px rgba(0, 0, 0, 0.075); border-radius: 16px;"
+	class="vl-card__shadow justify-center align-center flex-column pa-4 pa-md-8">
+	<CardTitle class="pa-0">
+		<h4 class="mb-0" style="font-weight: bold;">{title}</h4>
+	</CardTitle>
+	<CardText class="pa-0 mt-6 mb-6">
 		<slot/>
 	</CardText>
-	<CardActions>
+	<CardActions class="justify-center pa-0">
 		{#if previous}
-		<a href={previous} rel="noopener">
-			<Button title="Previous">▲</Button>
-		</a>
-		{/if}
-		{#if previous && next}
-			<div style="width: 16px"></div>
+			<a class="mr-4" href={previous} rel="noopener">
+				<Button title="Previous">
+					<Icon path={mdiArrowLeft} />
+					<span class="ml-2 d-none d-sm-block">{previous}</span>
+			</Button>
+			</a>
 		{/if}
 		{#if next}
-		<a href={next} rel="noopener">
-			<Button title="Next">▼</Button>
-		</a>
+			<a class="ml-4" style="flex: 1; text-align: right;"  href={next} rel="noopener">
+				<Button title="Next">
+					<span class="mr-2 d-none d-sm-block">{next}</span>
+					<Icon path={mdiArrowRight}/>
+				</Button>
+			</a>
 		{/if}
 	</CardActions>
 </Card>
