6611d89f7baa18b6f00fe8416c894727d297072b
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-01-30 00:28:07 +0300

    fix: event name duplicate bug is fixed
    Signed-off-by: abdullahcalisir12 <abdullahcalisir12@gmail.com>
    Signed-off-by: hurelhuyag <hurelhuyag@gmail.com>

--- src/main/frontend/src/components/aggregates/Events.svelte
+++ src/main/frontend/src/components/aggregates/Events.svelte
@@ -22,12 +22,23 @@
 			};
 		})
 	}
+
+	const isEventNameUnique = (value) => {
+    const result = events.filter(event => event.name === value);
+		return result.length === 1 ? undefined : 'Name must be unique';
+  }
+  
+  $: if (events) {
+    events.forEach(event => {
+      isEventNameUnique(event.name);
+    })
+  }
 </script>
 
 <h5>Events:</h5>
 {#each events as event, i}
   <span class="d-flex">
-    <TextField class="ma-2" bind:value={event.name} rules={[requireRule, classNameRule]} validateOnBlur={!event.name}>Name</TextField>
+    <TextField class="ma-2" bind:value={event.name} rules={[requireRule, classNameRule, isEventNameUnique]} validateOnBlur={!event.name}>Name</TextField>
     <Select id="event-fields" mandatory disabled={!stateFields.length} multiple class="ma-2" items={formatArrayForSelect(stateFields.map(f => f.name))} bind:value={event.fields}>Fields</Select>
     <DeleteButton title="Delete Event" on:click={() => deleteEvent(i)}/>
   </span>
