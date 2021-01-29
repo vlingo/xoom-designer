07f5279e95b2b3e95e03ca757340bf912a30f406
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-01-30 00:28:40 +0300

    fix: stateFields duplicate name bug is fixed
    Signed-off-by: abdullahcalisir12 <abdullahcalisir12@gmail.com>

--- src/main/frontend/src/components/aggregates/StateFields.svelte
+++ src/main/frontend/src/components/aggregates/StateFields.svelte
@@ -9,14 +9,25 @@
   const stateFieldsTypes =  formatArrayForSelect(['int', 'double', 'String', 'float', 'short', 'byte', 'boolean', 'long', 'char']);
 
 	const addStateField = () => stateFields = stateFields.concat({ name: "", type: "" });
-	const deleteStateField = (index) => { stateFields.splice(index, 1); stateFields = stateFields; }
+  const deleteStateField = (index) => { stateFields.splice(index, 1); stateFields = stateFields; }
+  
+  const isFieldNameUnique = (value) => {
+    const result = stateFields.filter(field => field.name === value);
+		return result.length === 1 ? undefined : 'Name must be unique';
+  }
+
+  $: if (stateFields) {
+    stateFields.forEach(field => {
+      isFieldNameUnique(field.name);
+    })
+  }
 </script>
 
 <h5>State Fields:</h5>
 {#each stateFields as stateField, i}
   <span class="d-flex">
   <div style="max-width: 100%">
-    <TextField disabled={i<1} class="ma-2" bind:value={stateField.name} rules={[requireRule, identifierRule]}>Name</TextField>
+    <TextField disabled={i<1} class="ma-2" bind:value={stateField.name} rules={[requireRule, identifierRule, isFieldNameUnique ]}>Name</TextField>
   </div>
   <div style="max-width: 100%">
     <Select mandatory disabled={i<1} class="ma-2" items={stateFieldsTypes} bind:value={stateField.type}>Type</Select>
