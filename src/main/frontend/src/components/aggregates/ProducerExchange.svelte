8b285a9af095d9876b2f3ddbe2cae04eff5109d8
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-01-30 04:26:27 +0300

    producer exchange improvements
    Signed-off-by: abdullahcalisir12 <abdullahcalisir12@gmail.com>
    Signed-off-by: hurelhuyag <hurelhuyag@gmail.com>

--- src/main/frontend/src/components/aggregates/ProducerExchange.svelte
+++ src/main/frontend/src/components/aggregates/ProducerExchange.svelte
@@ -10,9 +10,13 @@
   export let disableSchemaGroup;
 </script>
 
-<h5>Producer Exchange:</h5>
-<span class="d-flex">
-  <TextField class="ma-2" bind:value={producerExchangeName}>Exchange Name</TextField>
-  <TextField class="ma-2" bind:value={schemaGroup} rules={[schemaGroupRule]} validateOnBlur={!schemaGroup} disabled={disableSchemaGroup}>Organization : Unit : Context</TextField>
-</span>
-<Select mandatory disabled={!events.length} multiple class="ma-2" items={formatArrayForSelect(events.map(e => e.name))} bind:value={outgoingEvents}>Domain Event</Select>
+<fieldset class="pa-6 pt-8 pb-8 mb-8" style="border: 1px solid rgba(0,0,0,0.15); border-radius: 10px;">
+  <legend>
+    <h6 class="ma-0 pl-3 pr-3">Producer Exchange</h6>
+  </legend>
+  <span class="d-flex">
+    <TextField class="mb-3 pb-3 mr-4" style="flex: 1;" bind:value={producerExchangeName}>Exchange Name</TextField>
+    <TextField class="mb-3 pb-3" style="flex: 1;" bind:value={schemaGroup} rules={[schemaGroupRule]} validateOnBlur={!schemaGroup} disabled={disableSchemaGroup}>Organization : Unit : Context</TextField>
+  </span>
+  <Select mandatory disabled={!events.length} multiple items={formatArrayForSelect(events.map(e => e.name))} bind:value={outgoingEvents}>Domain Event</Select>
+</fieldset>
