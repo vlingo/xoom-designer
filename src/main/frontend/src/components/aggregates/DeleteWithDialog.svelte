9b7b2be19839c622489f4e4a4c4d8072598ca1d5
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-01-31 01:55:42 +0300

    deleteWithDialog updates
    Signed-off-by: abdullahcalisir12 <abdullahcalisir12@gmail.com>
    Signed-off-by: hurelhuyag <hurelhuyag@gmail.com>

--- src/main/frontend/src/components/aggregates/DeleteWithDialog.svelte
+++ src/main/frontend/src/components/aggregates/DeleteWithDialog.svelte
@@ -7,24 +7,44 @@
 		Dialog,
 	} from "svelte-materialify/src";
 	import DeleteButton from "./DeleteButton.svelte";
+	import { createEventDispatcher } from "svelte";
+
+	const dispatch = createEventDispatcher();
 	
 	export let type;
+	export let color = "red";
 	let dialogActive = false;
 
 	const open = () => dialogActive = true;
 	const close = () => dialogActive = false;
+	const onDelete = () => {
+		dispatch('click');
+		close();
+	}
 </script>
 
-<DeleteButton title="Delete {type}" on:click={open} color=""/>
+<DeleteButton title="Delete {type}" on:click={open} bind:color/>
 	
 <Dialog persistent bind:active={dialogActive}>
 	<Card class="pa-3">
-		<div class="d-flex flex-column" style="height: 8rem">
-			<CardTitle>Do you really want to delete this {type}?</CardTitle>
+		<div class="d-flex flex-column">
+			<CardTitle>
+				Are you sure you want to delete this {type}?
+				<div class="small">
+					<slot />
+				</div>
+			</CardTitle>
 			<CardActions style="margin-top: auto" class="justify-space-around">
-				<Button on:click text class="red-text">Delete</Button>
+				<Button on:click={onDelete} text class="red-text">Delete</Button>
 				<Button on:click={close}>Cancel</Button>
 			</CardActions>
 		</div>
 	</Card>
 </Dialog>
+
+<style lang="scss" global>
+	.small {
+		font-size: 14px;
+		word-break: break-word;
+	}
+</style>
