141af53f72a39375f0a61e8f2e1f04f7e889bc7b
Danilo Ambrosio <danilo.ambrosio@gmail.com>
2021-03-16 17:21:59 -0300

    Project Settings payload

--- src/main/java/io/vlingo/xoom/starter/Configuration.java
+++ src/main/java/io/vlingo/xoom/starter/Configuration.java
@@ -11,6 +11,7 @@ import io.vlingo.xoom.codegen.template.dataobject.DataObjectGenerationStep;
 import io.vlingo.xoom.codegen.template.exchange.ExchangeGenerationStep;
 import io.vlingo.xoom.codegen.template.model.ModelGenerationStep;
 import io.vlingo.xoom.codegen.template.model.ValueObjectGenerationStep;
+import io.vlingo.xoom.codegen.template.projectgenerationsettings.ProjectSettingsGenerationStep;
 import io.vlingo.xoom.codegen.template.projections.ProjectionGenerationStep;
 import io.vlingo.xoom.codegen.template.resource.RestResourceGenerationStep;
 import io.vlingo.xoom.codegen.template.schemata.SchemataGenerationStep;
@@ -73,6 +74,7 @@ public class Configuration {
             new ExchangeGenerationStep(),
             new SchemataGenerationStep(),
             new BootstrapGenerationStep(),
+            new ProjectSettingsGenerationStep(),
             new ContentCreationStep()
     );
 
