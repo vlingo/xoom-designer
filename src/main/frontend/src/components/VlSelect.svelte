6b2491f2c84e8312c36a1e1e28f225a29e79d080
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-03-18 01:51:39 +0300

    VlSelect component is created
    Signed-off-by: abdullahcalisir12 <abdullahcalisir12@gmail.com>

---
+++ src/main/frontend/src/components/VlSelect.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { Select } from "svelte-materialify/src";
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+
+  export let value;
+  let previousValue = value;
+
+  $: if (previousValue !== value) {
+    previousValue = value;
+    dispatch('change', value);
+  }
+</script>
+
+<Select {...$$props} bind:value>Storage Type</Select>
+
