2cb18190b86b972006a8b3fdbebd5cf2abf72fa5
VaughnVernon <vaughn@kalele.io>
2021-03-19 15:12:15 -0700

    Ensure that a number was stored.

--- src/main/frontend/src/stores/utils.js
+++ src/main/frontend/src/stores/utils.js
@@ -9,7 +9,18 @@ function parseIfJson(val){
 }
 
 export function createLocalStore(key, initialValue) {
-	const localValue = process.browser && localStorage.getItem(key) ? parseIfJson(localStorage.getItem(key)) : initialValue;
+  let localValue = initialValue;
+
+  if (process.browser && localStorage.getItem(key)) {
+    maybeStoredValue = parseIfJson(localStorage.getItem(key));
+    if (!isNaN(maybeStoredValue)) {
+      if ((typeof maybeStoredValue === 'number' && isFinite(maybeStoredValue)) ||
+          (Object.prototype.toString.apply(maybeStoredValue) === '[object Number]')) {
+          localValue = Number(maybeStoredValue);
+      }
+    }
+  }
+
 	const { subscribe, set } = writable(localValue);
 
 	return {
