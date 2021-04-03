737b67881c67a5c6b719bcfa2e76e0d23176db7f
Danilo Ambrosio <danilo.ambrosio@gmail.com>
2021-04-03 11:40:38 -0300

    1.6.1-SNAPSHOT

--- src/main/java/io/vlingo/xoom/starter/Configuration.java
+++ src/main/java/io/vlingo/xoom/starter/Configuration.java
@@ -29,7 +29,7 @@ public class Configuration {
 
   public static final String MAVEN_WRAPPER_DIRECTORY = ".mvn";
   public static final String XOOM_DESIGNER_FILE_VERSION = "1.6.0";
-  private static final String XOOM_VERSION_PLACEHOLDER = "1.6.0";
+  private static final String XOOM_VERSION_PLACEHOLDER = "1.6.1-SNAPSHOT";
   private static final String HOME_ENVIRONMENT_VARIABLE = "VLINGO_XOOM_STARTER_HOME";
 
   public static final List<TaskExecutionStep> PROJECT_GENERATION_STEPS = Arrays.asList(
