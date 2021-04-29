9f99857d0c436735c7038d69e13ec9d366aed00b
Danilo Ambrosio <danilo.ambrosio@gmail.com>
2021-04-29 11:42:57 -0300

    PR approved
    Signed-off-by: Danilo Ambrosio <danilo.ambrosio@gmail.com>

--- src/main/frontend/src/routes/context.svelte
+++ src/main/frontend/src/routes/context.svelte
@@ -14,12 +14,9 @@
 
 <!-- add newbie tooltips -->
 
-{#if $settings && $settings.context}
 <CardForm title="Context" next="aggregates" bind:valid={$isValid.context}>
 	<TextField class="mb-4 pb-4" placeholder="com.example" bind:value={$settings.context.groupId} rules={[requireRule, packageRule]} validateOnBlur={!$settings.context.groupId}>Group Id</TextField>
 	<TextField class="mb-4 pb-4" placeholder="demo" bind:value={$settings.context.artifactId} rules={[requireRule, artifactRule]} validateOnBlur={!$settings.context.artifactId}>Artifact Id</TextField>
 	<TextField class="mb-4 pb-4" placeholder="1.0.0" bind:value={$settings.context.artifactVersion} rules={[requireRule, versionRule]} validateOnBlur={!$settings.context.artifactVersion}>Artifact Version</TextField>
 	<TextField class="mb-4 pb-4" placeholder="com.example.demo" bind:value={$settings.context.packageName} rules={[requireRule, packageRule]} validateOnBlur={!$settings.context.packageName}>Base Package Name</TextField>
 </CardForm>
-{/if}
-
