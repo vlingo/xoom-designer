5081b5cd67e369e17fcd94d623a5a9d3360bc506
hurelhuyag <hurelhuyag@gmail.com>
2021-04-30 09:20:05 +0800

    ReactJS project generator reuse existing Freemarker configuration
    Signed-off-by: hurelhuyag <hurelhuyag@gmail.com>

--- src/main/java/io/vlingo/xoom/designer/task/reactjs/ReactJSCodeGenerator.java
+++ src/main/java/io/vlingo/xoom/designer/task/reactjs/ReactJSCodeGenerator.java
@@ -1,29 +1,17 @@
 package io.vlingo.xoom.designer.task.reactjs;
 
-import freemarker.cache.FileTemplateLoader;
-import freemarker.core.PlainTextOutputFormat;
 import freemarker.template.*;
 import io.vlingo.xoom.turbo.codegen.template.TemplateProcessorConfiguration;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.Writer;
-import java.util.Map;
 
 public class ReactJSCodeGenerator {
 
-    private final String templateRootPath = "/home/hurlee/IdeaProjects/vlingo-xoom/src/main/resources/codegen/reactjs/";
-    //private final Configuration configuration = TemplateProcessorConfiguration.instance().configuration;
-    private final Configuration configuration = new Configuration(Configuration.VERSION_2_3_30);
+    private final Configuration configuration = TemplateProcessorConfiguration.instance().configuration;
 
     public ReactJSCodeGenerator() throws IOException {
-        DefaultObjectWrapper objectWrapper = new DefaultObjectWrapper(Configuration.VERSION_2_3_30);
-        objectWrapper.setExposeFields(true);
-        configuration.setTemplateLoader(new FileTemplateLoader(new File(templateRootPath)));
-        configuration.setObjectWrapper(objectWrapper);
-        configuration.setOutputFormat(PlainTextOutputFormat.INSTANCE);
-        configuration.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
-
+        // TODO move into xoom-turbo module
         try{
             configuration.setSharedVariable("fns", new TemplateCustomFunctions());
         }catch (TemplateException e){
@@ -32,7 +20,7 @@ public class ReactJSCodeGenerator {
     }
 
     public void generateWith(String templatePath, Object arguments, Writer output) throws IOException, TemplateException {
-        Template template = configuration.getTemplate(templatePath + ".ftl");
+        Template template = configuration.getTemplate("/codegen/reactjs/" + templatePath + ".ftl");
         template.process(arguments, output);
     }
 }
