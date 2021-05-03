68d13293b6e98ded89ec5933e294c1d170ccb7ea
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-05-03 03:43:25 +0300

    placeholder texts are updated
    Signed-off-by: abdullahcalisir12 <abdullahcalisir12@gmail.com>

--- src/main/frontend/src/components/aggregates/Methods.svelte
+++ src/main/frontend/src/components/aggregates/Methods.svelte
@@ -50,10 +50,10 @@
             <TextField bind:value={method.name} rules={[requireRule, identifierRule, (v) => isPropertyUniqueRule(v, methods, 'name')]} validateOnBlur={!method.name}>Name</TextField>
           </div>
           <div class="mb-1 pb-1 mr-4" style="flex: 1;">
-            <Select disabled={!stateFields.length} multiple items={formatArrayForSelect(stateFields.map(f => f.name))} bind:value={method.parameters} placeholder="None">Parameters</Select>
+            <Select disabled={!stateFields.length} multiple items={formatArrayForSelect(stateFields.map(f => f.name))} bind:value={method.parameters} placeholder="(none)">Parameters</Select>
           </div>
           <div class="mb-1 pb-1 " style="flex: 1;">
-            <Select disabled={!events.length} items={formatArrayForSelect(events.map(e => e.name))} bind:value={method.event} placeholder="None">Event</Select>
+            <Select disabled={!events.length} items={formatArrayForSelect(events.map(e => e.name))} bind:value={method.event} placeholder="(none)">Event</Select>
           </div>
         </div>
         <div class="mb-3 pb-3 " style="flex: 1;">
