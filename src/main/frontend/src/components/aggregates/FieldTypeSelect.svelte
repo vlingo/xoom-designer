dea2721e88660c7e1ee85ea54e0a3d16fc1b6f21
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-05-07 03:31:23 +0300

    fieldTypeSelect component is created
    Signed-off-by: abdullahcalisir12 <abdullahcalisir12@gmail.com>

---
+++ src/main/frontend/src/components/aggregates/FieldTypeSelect.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { Select } from 'svelte-materialify/src';
+
+  export let mandatory;
+  export let disabled;
+  export let items;
+  export let value;
+  export let collectionType;
+
+  let innerValue = value;
+  let innerItems = [];
+
+  $: innerItems = collectionType ? items.map(item => ({
+    name: `${collectionType}<${item.value}>`,
+    value: item.value
+  })) : [...items];
+
+  function update(e) {
+    value = e.detail;
+  }
+</script>
+
+<Select {mandatory} {disabled} items={innerItems} bind:value={innerValue} on:change={(e) => update(e)}>
+  <slot />
+</Select>
