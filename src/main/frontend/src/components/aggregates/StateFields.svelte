0cb70c7e68ab66d79511a23befb6baf82f058ac9
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-05-17 00:06:43 +0300

    tooltip is added to state fields
    Signed-off-by: abdullahcalisir12 <abdullahcalisir12@gmail.com>

--- src/main/frontend/src/components/aggregates/StateFields.svelte
+++ src/main/frontend/src/components/aggregates/StateFields.svelte
@@ -7,6 +7,7 @@
 	import { settings, simpleTypes, collectionTypes } from '../../stores';
   import FillFieldsNote from './FillFieldsNote.svelte';
   import FieldTypeSelect from './FieldTypeSelect.svelte';
+  import ErrorWarningTooltip from './ErrorWarningTooltip.svelte';
 
   export let stateFields;
 
@@ -48,6 +49,12 @@
       </div>
       <div style="flex: 1;" class="mb-3 pb-4">
         <Select disabled={i === 0} items={formatArrayForSelect(collectionTypes.map(f => f.name))}  bind:value={stateField.collectionType} placeholder="(bare)">Collection</Select>
+      </div>
+      <div>
+        <ErrorWarningTooltip
+          names={['Name', 'Name', 'Name', 'Type']}
+          messages={[requireRule(stateField.name), identifierRule(stateField.name), isPropertyUniqueRule(stateField.name, stateFields, 'name') ,requireRule(stateField.type)]}
+        />
       </div>
       <div style="{stateFields.length > 1 ? 'width: 36px;' : ''}">
         {#if i !== 0}
