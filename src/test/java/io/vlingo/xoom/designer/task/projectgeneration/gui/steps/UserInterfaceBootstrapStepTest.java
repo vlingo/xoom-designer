066ee1457a0008b9671bd337f887a2122e73aa9e
Hamza JGUERIM <hamzajg@gmail.com>
2021-05-17 21:47:57 +0100

    Project gen from GraalVM native image

--- src/test/java/io/vlingo/xoom/designer/task/projectgeneration/gui/steps/UserInterfaceBootstrapStepTest.java
+++ src/test/java/io/vlingo/xoom/designer/task/projectgeneration/gui/steps/UserInterfaceBootstrapStepTest.java
@@ -1,37 +1,36 @@
 package io.vlingo.xoom.designer.task.projectgeneration.gui.steps;
 
-import io.vlingo.xoom.designer.ComponentRegistry;
 import io.vlingo.xoom.designer.Profile;
 import io.vlingo.xoom.designer.infrastructure.HomeDirectory;
 import io.vlingo.xoom.designer.infrastructure.Infrastructure;
 import io.vlingo.xoom.designer.task.TaskExecutionContext;
 import io.vlingo.xoom.designer.task.TaskExecutor;
 import io.vlingo.xoom.designer.task.projectgeneration.GenerationTarget;
+import io.vlingo.xoom.turbo.ComponentRegistry;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.*;
 
 public class UserInterfaceBootstrapStepTest {
 
-    @Test
-    public void testThatUserInterfaceBootstrapStepIsProcessed() {
-        new UserInterfaceBootstrapStep().process(TaskExecutionContext.withoutOptions());
-        Assertions.assertFalse(TaskExecutor.shouldExit());
-    }
+  @Test
+  public void testThatUserInterfaceBootstrapStepIsProcessed() {
+    new UserInterfaceBootstrapStep().process(TaskExecutionContext.withoutOptions());
+    Assertions.assertFalse(TaskExecutor.shouldExit());
+  }
 
-    @BeforeEach
-    public void setUp() {
-        Infrastructure.clear();
-        ComponentRegistry.clear();
-        Profile.enableTestProfile();
-        Infrastructure.resolveInternalResources(HomeDirectory.fromEnvironment());
-        ComponentRegistry.register(GenerationTarget.class, GenerationTarget.FILESYSTEM);
-    }
+  @BeforeEach
+  public void setUp() {
+    Infrastructure.clear();
+    ComponentRegistry.clear();
+    Profile.enableTestProfile();
+    Infrastructure.resolveInternalResources(HomeDirectory.fromEnvironment());
+    ComponentRegistry.register(GenerationTarget.class, GenerationTarget.FILESYSTEM);
+  }
 
-    @AfterAll
-    public static void clear() {
-        Profile.disableTestProfile();
-    }
+  @AfterAll
+  public static void clear() {
+    Profile.disableTestProfile();
+  }
 }
