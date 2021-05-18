a81bc740b61fab7e455a039e09f133b4a540d0ce
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-05-19 00:31:51 +0300

    varying collection mutation support methods is done
    Signed-off-by: abdullahcalisir12 <abdullahcalisir12@gmail.com>

--- src/main/frontend/src/components/aggregates/MethodForm.svelte
+++ src/main/frontend/src/components/aggregates/MethodForm.svelte
@@ -4,12 +4,21 @@
   import { formatArrayForSelect } from '../../utils';
   import ErrorWarningTooltip from "./ErrorWarningTooltip.svelte";
 
+  import List, { Item, Label } from '@smui/list';
+  import Checkbox from '@smui/checkbox';
+  import Menu, { SelectionGroup } from '@smui/menu';
+  import { Anchor } from '@smui/menu-surface';
+
   export let method;
   export let stateFields;
   export let events;
   export let methods;
 
   let selectedEvent = method.event ? [method.event] : [];
+  const symbols = ['*', '#', '+', '-']
+  let menu;
+  let anchor;
+  let anchorClasses = {};
 
   $: selectedEvent, onSelectedEventChanged();
 
@@ -17,6 +26,28 @@
     const lengthOfSelectedEvents = selectedEvent.length;
     method.event = lengthOfSelectedEvents > 0 ? selectedEvent[lengthOfSelectedEvents - 1] : '';
     selectedEvent = method.event ? [method.event] : [];
+  }
+
+  function updateParamaters(name, symbol) {
+    const nameWithOrWithoutSymbol = symbol ? `${name} ${symbol}` : name;
+    const indexOfAlreadyExists = method.parameters.findIndex(p => p === nameWithOrWithoutSymbol)
+    if (indexOfAlreadyExists > -1) {
+      method.parameters.splice(indexOfAlreadyExists, 1)
+      method.parameters = method.parameters
+      return;
+    }
+    var replace = `^${name}$|^${name} [>*#+-]$`;
+    var re = new RegExp(replace);
+    const indexOfAnyExists = method.parameters.findIndex(p => {
+      const a = p.search(re);
+      return a !== -1;
+    })
+    if (indexOfAnyExists > -1) {
+      method.parameters.splice(indexOfAnyExists, 1, nameWithOrWithoutSymbol)
+      method.parameters = method.parameters
+    } else {
+      method.parameters = [...method.parameters, nameWithOrWithoutSymbol]
+    }
   }
 
   $: validation = [requireRule(method.name), identifierRule(method.name), isPropertyUniqueRule(method.name, methods, 'name')].filter(v => v);
@@ -29,7 +60,73 @@
     <TextField bind:value={method.name} rules={[requireRule, identifierRule, (v) => isPropertyUniqueRule(v, methods, 'name')]} validateOnBlur={!method.name}>Name</TextField>
   </div>
   <div class="mb-1 pb-1 mr-4" style="flex: 1;">
-    <Select disabled={!stateFields.length} multiple items={formatArrayForSelect(stateFields.map(f => f.name))} bind:value={method.parameters} placeholder="(none)">Parameters</Select>
+    <div
+      class={Object.keys(anchorClasses).join(' ')}
+      use:Anchor={{
+        addClass: (className) => {
+          if (!anchorClasses[className]) {
+            anchorClasses[className] = true;
+          }
+        },
+        removeClass: (className) => {
+          if (anchorClasses[className]) {
+            delete anchorClasses[className];
+            anchorClasses = anchorClasses;
+          }
+        },
+      }}
+      bind:this={anchor}
+    >
+    <div on:click={() => menu.setOpen(true)}
+    >
+      <Select
+        value={method.parameters && method.parameters.length > 0 ? method.parameters.join(', ') : '(none)'}
+        disabled={!stateFields.length}
+      >
+        Parameters
+      </Select>
+    </div>
+    <Menu
+      bind:this={menu}
+      anchor={false}
+      bind:anchorElement={anchor}
+      anchorCorner="BOTTOM_LEFT"  
+      style="width: 100%;"
+    >
+      <List class="demo-list" checkList>
+        <SelectionGroup>
+          {#each stateFields as field}
+            <Item
+              class="pa-0"
+              on:SMUI:action={() => updateParamaters(field.name, field.collectionType ? '>' : '')}
+              selected={method.parameters.includes(field.collectionType ? `${field.name} >` : field.name)}
+            >
+              <Checkbox
+                checked={method.parameters.includes(field.collectionType ? `${field.name} >` : field.name)}
+                value={field.collectionType ? `${field.name} >` : field.name} />
+              <Label>{field.name} {field.collectionType ? '>' : ''}</Label>
+            </Item>
+            {#if field.collectionType}
+              <div>
+                {#each symbols as symbol}
+                  <Item
+                    class="pa-0 pl-10"
+                    on:SMUI:action={() => updateParamaters(field.name, symbol)}
+                    selected={method.parameters.includes(`${field.name} ${symbol}`)}
+                  >
+                    <Checkbox
+                      checked={method.parameters.includes(`${field.name} ${symbol}`)}
+                      value={`${field.name} ${symbol}`} />
+                    <Label>{field.name} {symbol}</Label>
+                  </Item>
+                {/each}
+              </div>
+            {/if}
+          {/each}
+        </SelectionGroup>
+      </List>
+    </Menu>
+    </div>
   </div>
   <div class="mb-1 pb-1 " style="flex: 1;">
     <Select disabled={!events.length} multiple closeOnClick={true} items={formatArrayForSelect(events.map(e => e.name))} bind:value={selectedEvent} placeholder="(none)">Event</Select>
