1ba8b19151d5558b4bbe531fc37632e75cc04eba
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-05-18 14:51:46 +0300

    sortable wrapper component is created
    Signed-off-by: abdullahcalisir12 <abdullahcalisir12@gmail.com>

---
+++ src/main/frontend/src/components/Sortable.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { onMount } from "svelte";
+  import Sortable from 'sortablejs';
+
+  export let options = {};
+  let sortable;
+
+  onMount(() => {
+		Sortable.create(sortable, {
+      filter: '.disabled',
+      onMove: event => {
+        return !event.related.classList.contains('disabled');
+      },
+      handle: '.handle',
+			animation: 100,
+      ...options
+		});
+	});
+</script>
+
+<div bind:this={sortable}>
+  <slot />
+</div>
