07d9ecbb05053eec89d62f2ca23b4e0f108e2010
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-05-25 01:13:30 +0300

    state field component is added to state fields component and id order bug is fixed
    Signed-off-by: abdullahcalisir12 <abdullahcalisir12@gmail.com>

--- src/main/frontend/src/components/aggregates/StateFields.svelte
+++ src/main/frontend/src/components/aggregates/StateFields.svelte
@@ -1,14 +1,9 @@
 <script>
-  import { Select, TextField, Icon } from 'svelte-materialify/src';
-  import DeleteWithDialog from "./DeleteWithDialog.svelte";
 	import CreateButton from "./CreateButton.svelte";
-  import { identifierRule, requireRule, isPropertyUniqueRule } from "../../validators";
   import { formatArrayForSelect, uuid } from '../../utils';
-	import { settings, simpleTypes, collectionTypes } from '../../stores';
-  import FieldTypeSelect from './FieldTypeSelect.svelte';
-  import ErrorWarningTooltip from './ErrorWarningTooltip.svelte';
+	import { settings, simpleTypes } from '../../stores';
   import Sortable from '../Sortable.svelte';
-  import { mdiArrowUpDown, mdiArrowVerticalLock } from '@mdi/js';
+  import StateField from './StateField.svelte';
 
   export let stateFields;
 
@@ -31,56 +26,20 @@
   <legend>
     <h6 class="ma-0 pl-3 pr-3">State Fields</h6>
   </legend>
+  <StateField {stateFields} stateField={stateFields[0]} i={0} {stateFieldsTypes} />
   <Sortable
     options={{
 			onSort: (d) => {
-        const [field] = stateFields.splice(d.oldIndex, 1);
-        stateFields.splice(d.newIndex === 0 ? 1 : d.newIndex, 0, field);
+        const [field] = stateFields.splice(d.oldIndex + 1, 1);
+        stateFields.splice(d.newIndex + 1, 0, field);
         stateFields = stateFields;
 			},
     }}
   >
   {#each stateFields as stateField, i (stateField.uid)}
-    <div class="d-flex">
-      <div
-        class="handle pa-2"
-        class:disabled={i === 0}
-        style="width: 42px; cursor: {i === 0 ? 'not-allowed' : 'move'};"
-      >
-        <Icon path={i === 0 ? mdiArrowVerticalLock : mdiArrowUpDown}/>
-      </div>
-      <div style="flex: 1;" class="mb-3 pb-4 mr-4">
-        <TextField disabled={i === 0} autocomplete="off" bind:value={stateField.name} rules={[requireRule, identifierRule, (v) => isPropertyUniqueRule(v, stateFields, 'name') ]}>Name</TextField>
-      </div>
-      <div style="flex: 1;" class="mb-3 pb-4 mr-4">
-        <FieldTypeSelect
-          mandatory
-          disabled={i === 0}
-          items={stateFieldsTypes}
-          bind:value={stateField.type}
-          collectionType={stateField.collectionType}
-        >
-          Type
-        </FieldTypeSelect>
-        <!-- <Select mandatory disabled={i === 0} items={stateFieldsTypes} bind:value={stateField.type}>Type</Select> -->
-      </div>
-      <div style="flex: 1;" class="mb-3 pb-4">
-        <Select disabled={i === 0} items={formatArrayForSelect(collectionTypes.map(f => f.name))}  bind:value={stateField.collectionType} placeholder="(bare)">Collection</Select>
-      </div>
-      <div>
-        <ErrorWarningTooltip
-          names={['Name', 'Name', 'Name', 'Type']}
-          messages={[requireRule(stateField.name), identifierRule(stateField.name), isPropertyUniqueRule(stateField.name, stateFields, 'name') ,requireRule(stateField.type)]}
-        />
-      </div>
-      <div style="{stateFields.length > 1 ? 'width: 36px;' : ''}">
-        {#if i !== 0}
-          <DeleteWithDialog type="State Field" on:click={() => deleteStateField(i)}>
-            <b>{stateField.name}</b> might be in use at events or methods sections.
-          </DeleteWithDialog>
-        {/if}
-      </div>
-    </div>
+    {#if i > 0}
+      <StateField {stateFields} bind:stateField i={i+1} {stateFieldsTypes} on:delete={() => deleteStateField(i+1)} />
+    {/if}
   {/each}
   </Sortable>
   <CreateButton title="Add State Field" on:click={addStateField}/>
