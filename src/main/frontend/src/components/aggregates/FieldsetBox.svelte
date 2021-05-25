fae6735056ae2b2391258c7df0ea6b99e9e200cd
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-05-25 06:54:03 +0300

    fieldsetBox component is created
    Signed-off-by: abdullahcalisir12 <abdullahcalisir12@gmail.com>

---
+++ src/main/frontend/src/components/aggregates/FieldsetBox.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  import CreateButton from "./CreateButton.svelte";
+  export let title = "";
+
+  const dispatch = createEventDispatcher();
+  function add() {
+    dispatch('add')
+  }
+</script>
+<fieldset class="pa-6 pt-8 pb-8 mb-8" style="border: 1px solid rgba(0,0,0,0.15); border-radius: 10px;">
+  <legend>
+    <h6 class="ma-0 pl-3 pr-3">{title}</h6>
+  </legend>
+  <slot />
+  <CreateButton title="Add" on:click={add} />
+</fieldset>
