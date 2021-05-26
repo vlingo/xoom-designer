ea62b0cb5fd9d313e58c1c87a5a1257641ad5fd5
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-05-27 01:38:13 +0300

    routes ui updates
    Signed-off-by: abdullahcalisir12 <abdullahcalisir12@gmail.com>

--- src/main/frontend/src/components/aggregates/Routes.svelte
+++ src/main/frontend/src/components/aggregates/Routes.svelte
@@ -1,26 +1,41 @@
 <script>
-	import { TextField } from 'svelte-materialify/src';
+  import Textfield from '@smui/textfield/Textfield.svelte';
   import { requireRule, routeRule } from "../../validators";
   import ErrorWarningTooltip from './ErrorWarningTooltip.svelte';
   import FieldsetBox from './FieldsetBox.svelte';
-
   import Route from "./Route.svelte";
+  import { tick } from 'svelte';
+
   export let rootPath;
   export let routes;
   export let methods;
 
   const addRoute = () => {
     routes = [...routes, { path: "", httpMethod: "GET", aggregateMethod: "", requireEntityLoad: false }]
+    tick().then(() => {
+      const el = document.querySelector(`#routePath${routes.length - 1} input`);
+      if (el) el.focus()
+    })
   };
   const deleteRoute = (index) => {
     routes.splice(index, 1);
     routes = [...routes];
+    tick().then(() => {
+      const el = document.querySelector(`#routePath${index === 0 ? 0 : index - 1} input`);
+      if (el) el.focus()
+    })
   }
 </script>
 
 <FieldsetBox title="API" on:add={addRoute}>
-  <div>
-    <TextField class="mb-3 pb-3" bind:value={rootPath} rules={[requireRule, routeRule]} validateOnBlur={!rootPath}>Root Path</TextField>
+  <div class="mb-4">
+    <Textfield
+      style="width: 100%;"
+      label="Root Path"
+      required
+      bind:value={rootPath}
+      invalid={[requireRule(rootPath), routeRule(rootPath)].some(f => f)}
+    />
     <ErrorWarningTooltip
       names={['Root path', 'Root path']}
       messages={[requireRule(rootPath), routeRule(rootPath)]}
@@ -28,6 +43,6 @@
   </div>
 
   {#each routes as route, i}
-    <Route bind:route bind:methods on:delete={() => deleteRoute(i)}/>
+    <Route bind:route bind:methods {i} on:delete={() => deleteRoute(i)}/>
   {/each}
 </FieldsetBox>
