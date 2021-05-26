2e12270373b598113375493cb537592828ab6737
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-05-27 01:37:24 +0300

    pathfield ui updates
    Signed-off-by: abdullahcalisir12 <abdullahcalisir12@gmail.com>

--- src/main/frontend/src/components/aggregates/PathField.svelte
+++ src/main/frontend/src/components/aggregates/PathField.svelte
@@ -1,13 +1,21 @@
 <script>
-	import { TextField } from "svelte-materialify/src";
+	import Textfield from "@smui/textfield/Textfield.svelte";
 	import { requireRule } from "../../validators";
 	import ErrorWarningTooltip from "./ErrorWarningTooltip.svelte";
 
 	export let path = "";
+	export let i;
 </script>
 
-<div class="d-flex">
-	<TextField bind:value={path} rules={[requireRule]} validateOnBlur={!path}>Path</TextField>
+<div class="d-flex align-center">
+	<Textfield
+		id="routePath{i}"
+		style="flex: 1;"
+		label="Path"
+		required
+		bind:value={path}
+		invalid={[requireRule(path)].some(f => f)}
+	/>
 	<ErrorWarningTooltip
 		type='warning'
 		messages={[path.includes('{id}') ? '' : 'Requires {id}?']}
