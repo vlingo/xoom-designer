6821182b0e9833d3e5e7a1f3e9ccabc7d5afb472
abdullahcalisir12 <abdullahcalisir12@gmail.com>
2021-05-27 01:35:54 +0300

    fieldbox add button hide prop is added
    Signed-off-by: abdullahcalisir12 <abdullahcalisir12@gmail.com>

--- src/main/frontend/src/components/aggregates/FieldsetBox.svelte
+++ src/main/frontend/src/components/aggregates/FieldsetBox.svelte
@@ -2,6 +2,7 @@
   import { createEventDispatcher } from "svelte";
   import CreateButton from "./CreateButton.svelte";
   export let title = "";
+  export let addable = true;
 
   const dispatch = createEventDispatcher();
   function add() {
@@ -13,5 +14,7 @@
     <h6 class="ma-0 pl-3 pr-3">{title}</h6>
   </legend>
   <slot />
-  <CreateButton title="Add" on:click={add} />
+  {#if addable}
+    <CreateButton title="Add" on:click={add} />
+  {/if}
 </fieldset>
