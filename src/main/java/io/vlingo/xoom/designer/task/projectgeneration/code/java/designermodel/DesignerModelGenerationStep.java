b673029e3b3924eee8e64c2bf9dc1b6c78b7f041
Danilo Ambrosio <danilo.ambrosio@gmail.com>
2021-06-24 09:46:39 -0300

    Fix package name

--- src/main/java/io/vlingo/xoom/designer/task/projectgeneration/code/java/designermodel/DesignerModelGenerationStep.java
+++ src/main/java/io/vlingo/xoom/designer/task/projectgeneration/code/java/designermodel/DesignerModelGenerationStep.java
@@ -5,7 +5,7 @@
 // was not distributed with this file, You can obtain
 // one at https://mozilla.org/MPL/2.0/.
 
-package io.vlingo.xoom.designer.task.projectgeneration.code.java.projectgenerationsettings;
+package io.vlingo.xoom.designer.task.projectgeneration.code.java.designermodel;
 
 import io.vlingo.xoom.codegen.CodeGenerationContext;
 import io.vlingo.xoom.codegen.template.TemplateData;
--- src/main/java/io/vlingo/xoom/designer/task/projectgeneration/code/java/designermodel/DesignerModelTemplateData.java
+++ src/main/java/io/vlingo/xoom/designer/task/projectgeneration/code/java/designermodel/DesignerModelTemplateData.java
@@ -4,7 +4,7 @@
 // Mozilla Public License, v. 2.0. If a copy of the MPL
 // was not distributed with this file, You can obtain
 // one at https://mozilla.org/MPL/2.0/.
-package io.vlingo.xoom.designer.task.projectgeneration.code.java.projectgenerationsettings;
+package io.vlingo.xoom.designer.task.projectgeneration.code.java.designermodel;
 
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
