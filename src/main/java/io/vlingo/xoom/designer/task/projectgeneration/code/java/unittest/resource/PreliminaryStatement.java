2f4c2d3f72d557310bffd824d605531fb3ed610b
Hamza JGUERIM <hamzajg@gmail.com>
2021-08-15 22:10:14 +0100

    Refactoring..
    Signed-off-by: Hamza JGUERIM <hamzajg@gmail.com>

--- src/main/java/io/vlingo/xoom/designer/task/projectgeneration/code/java/unittest/resource/PreliminaryStatement.java
+++ src/main/java/io/vlingo/xoom/designer/task/projectgeneration/code/java/unittest/resource/PreliminaryStatement.java
@@ -8,70 +8,85 @@ package io.vlingo.xoom.designer.task.projectgeneration.code.java.unittest.resour
 
 import java.util.Collections;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PreliminaryStatement {
 
-	private static final String REST_ASSURED_READ = "%s result = given()%n" +
-			"        .when()%n" +
-			"        .get(\"%s\")%n" +
-			"        .then()%n" +
-			"        .statusCode(200)%n" +
-			"        .extract()%n" +
-			"        .body()%n" +
-			"        .as(%s.class);%n";
-	private static final String REST_ASSURED_READ_ALL = "%s[] result = given()%n" +
-			"        .when()%n" +
-			"        .get(\"%s\")%n" +
-			"        .then()%n" +
-			"        .statusCode(200)%n" +
-			"        .extract()%n" +
-			"        .body()%n" +
-			"        .as(%s[].class);%n";
-	private static final String REST_ASSURED_WRITE = "%s result = given()%n" +
-			"        .when()%n" +
-			"        .body(%s)%n" +
-			"        .%s(\"%s\")%n" +
-			"        .then()%n" +
-			"        .statusCode(%s)%n" +
-			"        .extract()%n" +
-			"        .body()%n" +
-			"        .as(%s.class);%n";
-	private static final String REST_ASSURED_DELETE = "given()%n" +
-			"        .when()%n" +
-			"        .%s(\"%s\")%n" +
-			"        .then()%n" +
-			"        .statusCode(200);%n";
+  private static final String REST_ASSURED_READ = "%s result = given()%n" +
+      "        .when()%n" +
+      "        .get(\"%s\")%n" +
+      "        .then()%n" +
+      "        .statusCode(200)%n" +
+      "        .extract()%n" +
+      "        .body()%n" +
+      "        .as(%s.class);%n";
+  private static final String REST_ASSURED_READ_ALL = "%s[] result = given()%n" +
+      "        .when()%n" +
+      "        .get(\"%s\")%n" +
+      "        .then()%n" +
+      "        .statusCode(200)%n" +
+      "        .extract()%n" +
+      "        .body()%n" +
+      "        .as(%s[].class);%n";
+  private static final String REST_ASSURED_WRITE = "%s result = given()%n" +
+      "        .when()%n" +
+      "        .body(%s)%n" +
+      "        .%s(\"%s\")%n" +
+      "        .then()%n" +
+      "        .statusCode(%s)%n" +
+      "        .extract()%n" +
+      "        .body()%n" +
+      "        .as(%s.class);%n";
+  private static final String REST_ASSURED_DELETE = "given()%n" +
+      "        .when()%n" +
+      "        .%s(\"%s\")%n" +
+      "        .then()%n" +
+      "        .statusCode(200);%n";
 
-	public static List<String> with(final String aggregateUriRoot, final String valueObjectData, final String rootPath, final String rootMethod) {
-		final String testDataVariableName = TestDataFormatter.formatLocalVariableName(1);
-		String rootPathWithId = rootPath.replace("{id}", "\"+" + testDataVariableName + ".id" + "+\"");
-		switch (rootMethod) {
-			case "get":
-				if (rootPath.equals(aggregateUriRoot))
-					return fetchAll(valueObjectData, rootPathWithId);
-				return fetchById(valueObjectData, rootPathWithId);
-			case "delete":
-				return deleteById(rootPathWithId, rootMethod);
-			default:
-				return writeData(valueObjectData, rootPathWithId, rootMethod, testDataVariableName);
-		}
-	}
+  public static List<String> with(final String aggregateUriRoot, final String valueObjectData, final String rootPath, final String rootMethod) {
+    final String testDataVariableName = TestDataFormatter.formatLocalVariableName(1);
+    String rootPathWithId = replaceRootPathWithIds(rootPath, testDataVariableName);
 
-	private static List<String> writeData(String valueObjectData, String rootPath, String rootMethod, String testDataVariableName) {
-		return Collections.singletonList(String.format(REST_ASSURED_WRITE, valueObjectData, testDataVariableName, rootMethod,
-				rootPath, rootMethod.equals("post") ? 201 : 200, valueObjectData));
-	}
+    switch (rootMethod) {
+      case "get":
+        if (rootPath.equals(aggregateUriRoot))
+          return fetchAll(valueObjectData, rootPathWithId);
+        return fetchById(valueObjectData, rootPathWithId);
+      case "delete":
+        return deleteById(rootPathWithId, rootMethod);
+      default:
+        return writeData(valueObjectData, rootPathWithId, rootMethod, testDataVariableName);
+    }
+  }
 
-	private static List<String> deleteById(String rootPath, String rootMethod) {
-		return Collections.singletonList(String.format(REST_ASSURED_DELETE, rootMethod, rootPath));
-	}
+  private static String replaceRootPathWithIds(String rootPath, String testDataVariableName) {
+    final String regex = "(\\{([^{]+)})";
+    final Pattern pattern = Pattern.compile(regex);
+    Matcher matcher = pattern.matcher(rootPath);
+    String result = rootPath;
+    while (matcher.find()) {
+      String replaceWith = "\"+" + testDataVariableName + "." + matcher.group(2) + "+\"";
+      result = result.replace(matcher.group(0), replaceWith);
+    }
+    return result;
+  }
 
-	private static List<String> fetchById(String valueObjectData, String rootPath) {
-		return Collections.singletonList(String.format(REST_ASSURED_READ, valueObjectData, rootPath, valueObjectData));
-	}
+  private static List<String> writeData(String valueObjectData, String rootPath, String rootMethod, String testDataVariableName) {
+    return Collections.singletonList(String.format(REST_ASSURED_WRITE, valueObjectData, testDataVariableName, rootMethod,
+        rootPath, rootMethod.equals("post") ? 201 : 200, valueObjectData));
+  }
 
-	private static List<String> fetchAll(String valueObjectData, String rootPath) {
-		return Collections.singletonList(String.format(REST_ASSURED_READ_ALL, valueObjectData, rootPath, valueObjectData));
-	}
+  private static List<String> deleteById(String rootPath, String rootMethod) {
+    return Collections.singletonList(String.format(REST_ASSURED_DELETE, rootMethod, rootPath));
+  }
+
+  private static List<String> fetchById(String valueObjectData, String rootPath) {
+    return Collections.singletonList(String.format(REST_ASSURED_READ, valueObjectData, rootPath, valueObjectData));
+  }
+
+  private static List<String> fetchAll(String valueObjectData, String rootPath) {
+    return Collections.singletonList(String.format(REST_ASSURED_READ_ALL, valueObjectData, rootPath, valueObjectData));
+  }
 
 }
