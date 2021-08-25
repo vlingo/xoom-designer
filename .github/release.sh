a51076962024549248443b82b392c822808229aa
Jakub Zalas <jakub@zalas.pl>
2021-08-25 13:52:58 +0100

    Only update docker-compose on releases (not snapshots)

--- .github/release.sh
+++ .github/release.sh
@@ -9,8 +9,10 @@ $SCRIPT_PATH/update-version.sh $VERSION
 
 sed -i'.bkp' -e '/<artifactId>xoom-[a-z\-]*<\/artifactId>/{' -e 'n;s/\(<version>\)[0-9\.]*\(<\/version>\)/\1'$VERSION'\2/' -e '}' -e 's/\(io.vlingo.xoom:xoom-[a-z\-]*\):[0-9\.]*/\1:'$VERSION'/' README.md
 sed -i'.bkp' -E 's/(xoom-designer-)[1-9]*\.[0-9]*\.[0-9]*\.jar/\1'$VERSION'.jar/g' README.md
+sed -i'.bkp' -E 's/(image: vlingo\/xoom-designer:)[1-9]*\.[0-9]*\.[0-9]*/\1'$VERSION'/g' docker-compose.yml
+sed -i'.bkp' -E 's/(image: vlingo\/xoom-schemata:)[1-9]*\.[0-9]*\.[0-9]*/\1'$VERSION'/g' docker-compose.yml
 
-git add README.md
+git add README.md docker-compose.yml
 git commit -m "Release v$VERSION"
 git tag "$VERSION" -m "Release v$VERSION"
 git push --follow-tags
--- .github/update-version.sh
+++ .github/update-version.sh
@@ -7,14 +7,11 @@ VERSION=${1}
 mvn versions:set -DnewVersion=$VERSION
 mvn versions:use-dep-version -Dincludes=io.vlingo.xoom -DdepVersion=$VERSION -DforceVersion=true
 
-sed -i'.bkp' -E 's/(image: vlingo\/xoom-designer:)[1-9]*\.[0-9]*\.[0-9]*/\1'$VERSION'/g' docker-compose.yml
-sed -i'.bkp' -E 's/(image: vlingo\/xoom-schemata:)[1-9]*\.[0-9]*\.[0-9]*/\1'$VERSION'/g' docker-compose.yml
 sed -i'.bkp' -E 's/assertEquals\("[1-9]*\.[0-9]*\.[0-9]*(-SNAPSHOT)?"/assertEquals("'$VERSION'"/g' src/test/java/io/vlingo/xoom/designer/infrastructure/restapi/data/TaskExecutionContextMapperTest.java
 sed -i'.bkp' -E 's/XOOM_VERSION_PLACEHOLDER = "[1-9]*\.[0-9]*\.[0-9]*(-SNAPSHOT)?"/XOOM_VERSION_PLACEHOLDER = "'$VERSION'"/g' src/main/java/io/vlingo/xoom/designer/Configuration.java
 
 git add \
   pom.xml \
-  docker-compose.yml \
   src/test/java/io/vlingo/xoom/designer/infrastructure/restapi/data/TaskExecutionContextMapperTest.java \
   src/main/java/io/vlingo/xoom/designer/Configuration.java
 
