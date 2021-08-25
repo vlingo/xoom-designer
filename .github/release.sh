208c79f62b5f345340c3735494e7b7fbcfd0ebf5
Jakub Zalas <jakub@zalas.pl>
2021-08-25 13:50:43 +0100

    Only update the README on releases (not snapshots)

--- .github/release.sh
+++ .github/release.sh
@@ -7,6 +7,10 @@ SCRIPT_PATH=$(cd -- "$(dirname "$0")/" >/dev/null 2>&1 ; pwd -P)
 
 $SCRIPT_PATH/update-version.sh $VERSION
 
+sed -i'.bkp' -e '/<artifactId>xoom-[a-z\-]*<\/artifactId>/{' -e 'n;s/\(<version>\)[0-9\.]*\(<\/version>\)/\1'$VERSION'\2/' -e '}' -e 's/\(io.vlingo.xoom:xoom-[a-z\-]*\):[0-9\.]*/\1:'$VERSION'/' README.md
+sed -i'.bkp' -E 's/(xoom-designer-)[1-9]*\.[0-9]*\.[0-9]*\.jar/\1'$VERSION'.jar/g' README.md
+
+git add README.md
 git commit -m "Release v$VERSION"
 git tag "$VERSION" -m "Release v$VERSION"
 git push --follow-tags
--- .github/update-version.sh
+++ .github/update-version.sh
@@ -7,8 +7,6 @@ VERSION=${1}
 mvn versions:set -DnewVersion=$VERSION
 mvn versions:use-dep-version -Dincludes=io.vlingo.xoom -DdepVersion=$VERSION -DforceVersion=true
 
-sed -i'.bkp' -e '/<artifactId>xoom-[a-z\-]*<\/artifactId>/{' -e 'n;s/\(<version>\)[0-9\.]*\(<\/version>\)/\1'$VERSION'\2/' -e '}' -e 's/\(io.vlingo.xoom:xoom-[a-z\-]*\):[0-9\.]*/\1:'$VERSION'/' README.md
-sed -i'.bkp' -E 's/(xoom-designer-)[1-9]*\.[0-9]*\.[0-9]*\.jar/\1'$VERSION'.jar/g' README.md
 sed -i'.bkp' -E 's/(image: vlingo\/xoom-designer:)[1-9]*\.[0-9]*\.[0-9]*/\1'$VERSION'/g' docker-compose.yml
 sed -i'.bkp' -E 's/(image: vlingo\/xoom-schemata:)[1-9]*\.[0-9]*\.[0-9]*/\1'$VERSION'/g' docker-compose.yml
 sed -i'.bkp' -E 's/assertEquals\("[1-9]*\.[0-9]*\.[0-9]*(-SNAPSHOT)?"/assertEquals("'$VERSION'"/g' src/test/java/io/vlingo/xoom/designer/infrastructure/restapi/data/TaskExecutionContextMapperTest.java
@@ -16,7 +14,6 @@ sed -i'.bkp' -E 's/XOOM_VERSION_PLACEHOLDER = "[1-9]*\.[0-9]*\.[0-9]*(-SNAPSHOT)
 
 git add \
   pom.xml \
-  README.md \
   docker-compose.yml \
   src/test/java/io/vlingo/xoom/designer/infrastructure/restapi/data/TaskExecutionContextMapperTest.java \
   src/main/java/io/vlingo/xoom/designer/Configuration.java
