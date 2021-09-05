3717ce3d13dc9fd5dbe759f9fbd515bc7d6ba3b0
Jakub Zalas <jakub@zalas.pl>
2021-09-05 16:56:08 +0100

    Update the version in README

--- .github/next-dev.sh
+++ .github/next-dev.sh
@@ -7,6 +7,9 @@ SCRIPT_PATH=$(cd -- "$(dirname "$0")/" >/dev/null 2>&1 ; pwd -P)
 
 $SCRIPT_PATH/update-version.sh $VERSION
 
+sed -i'.bkp' -e 's/[1-9]*.[0-9]*.[0-9]*-SNAPSHOT/'$VERSION'/g' README.md
+
+git add README.md
 git commit -m "Next development version $VERSION"
 git push --follow-tags
 
