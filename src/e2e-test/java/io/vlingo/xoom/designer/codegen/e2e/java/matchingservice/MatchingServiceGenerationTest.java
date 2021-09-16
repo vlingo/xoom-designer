e975100a94a6ddfe70e417dc8048dcb29cd3cbb4
Danilo Ambrosio <danilo.ambrosio@gmail.com>
2021-09-16 13:09:47 -0300

    Matching context

---
+++ src/e2e-test/java/io/vlingo/xoom/designer/codegen/e2e/java/matchingservice/MatchingServiceGenerationTest.java
@@ -0,0 +1,54 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+package io.vlingo.xoom.designer.codegen.e2e.java.matchingservice;
+
+import io.vlingo.xoom.designer.codegen.e2e.Project;
+import io.vlingo.xoom.designer.codegen.e2e.SupportingServicesManager;
+import io.vlingo.xoom.designer.codegen.e2e.java.JavaProjectGenerationTest;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.vlingo.xoom.designer.codegen.e2e.SupportingServicesManager.RABBIT_MQ;
+import static io.vlingo.xoom.designer.codegen.e2e.SupportingServicesManager.SCHEMATA;
+
+/**
+ * This scenario consists of a service containing complex values objects
+ * and command methods accepting many combination of parameters.
+ */
+public class MatchingServiceGenerationTest extends JavaProjectGenerationTest {
+
+  @BeforeAll
+  public static void setUp() {
+    SupportingServicesManager.run();
+    init();
+  }
+
+  /**
+   * Test that the upstream service is generated and working with:
+   * - Sourced Entities containing Value Objects
+   * - Producer Exchanges / Schema Registration
+   * - Event-based projection
+   * - Without Annotations and Auto Dispatch
+   */
+  @Test
+  public void testThatGeneratedServicesAreWorking() {
+    assertServiceIsAvailable(SupportingServicesManager.findPortOf(SCHEMATA), "Schemata service is not available");
+    assertServiceIsAvailable(SupportingServicesManager.findPortOf(RABBIT_MQ), "RabbitMQ service is not available");
+
+    final Project matchingServiceWithSourcedEntities =
+            Project.from("matching-context", "matching-with-sourced-entities");
+
+    generateAndRun(matchingServiceWithSourcedEntities);
+  }
+
+  @AfterAll
+  public static void tearDown() throws Exception {
+    clear();
+  }
+
+}
