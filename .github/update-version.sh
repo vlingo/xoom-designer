a475d5db42ac9c52d88b318a140a2ee01b2ef95e
Jakub Zalas <jakub@zalas.pl>
2022-02-02 10:34:22 +0000

    Do not update the version during the release anymore (replaced by #127)
    Replaced by changes in 8c3b0a5d9f5caeeaa74264330095a9123137c664
    
    Signed-off-by: Jakub Zalas <jakub@zalas.pl>

--- .github/update-version.sh
+++ .github/update-version.sh
@@ -7,11 +7,4 @@ VERSION="$1"
 mvn versions:set -DnewVersion=$VERSION -DgenerateBackupPoms=false
 mvn versions:use-dep-version -Dincludes=io.vlingo.xoom -DdepVersion=$VERSION -DforceVersion=true -DgenerateBackupPoms=false
 
-sed -i'.bkp' -E 's/assertEquals\("[1-9]*\.[0-9]*\.[0-9]*(-SNAPSHOT)?"/assertEquals("'$VERSION'"/g' src/test/java/io/vlingo/xoom/designer/infrastructure/restapi/data/CodeGenerationContextMapperTest.java
-sed -i'.bkp' -E 's/XOOM_VERSION_PLACEHOLDER = "[1-9]*\.[0-9]*\.[0-9]*(-SNAPSHOT)?"/XOOM_VERSION_PLACEHOLDER = "'$VERSION'"/g' src/main/java/io/vlingo/xoom/designer/Configuration.java
-
-git add \
-  pom.xml \
-  src/test/java/io/vlingo/xoom/designer/infrastructure/restapi/data/CodeGenerationContextMapperTest.java \
-  src/main/java/io/vlingo/xoom/designer/Configuration.java
-
+git add pom.xml
