c7e7c0b7caf3d30ea0fe2a0785fd8c9ccec1eb0a
Hamza JGUERIM <hamzajg@gmail.com>
2022-02-04 19:14:33 +0100

    Refactoring..
    Signed-off-by: Hamza JGUERIM <hamzajg@gmail.com>

--- src/main/java/io/vlingo/xoom/designer/codegen/java/unittest/resource/TestCase.java
+++ src/main/java/io/vlingo/xoom/designer/codegen/java/unittest/resource/TestCase.java
@@ -22,101 +22,113 @@ import java.util.stream.Collectors;
 
 public class TestCase {
 
-    public static final int TEST_DATA_SET_SIZE = 2;
+  public static final int TEST_DATA_SET_SIZE = 2;
 
-    private final String methodName;
-    private final String dataDeclaration;
-    private final List<TestStatement> statements = new ArrayList<>();
-    private final List<String> preliminaryStatements = new ArrayList<>();
-    private final String rootMethod;
+  private final String methodName;
+  private final String dataDeclaration;
+  private final List<TestStatement> statements = new ArrayList<>();
+  private final List<String> preliminaryStatements = new ArrayList<>();
+  private final String rootMethod;
 
-		private final int urlPathCount;
-    private CollectionMutation collectionMutation;
+  private final int urlPathCount;
+  private CollectionMutation collectionMutation;
 
-    private boolean isModelHasSelfDescribingEvents;
+  private boolean isModelHasSelfDescribingEvents;
 
-    public static List<TestCase> from(final CodeGenerationParameter aggregate, List<CodeGenerationParameter> valueObjects) {
-        return aggregate.retrieveAllRelated(Label.ROUTE_SIGNATURE)
-                .map(signature -> new TestCase(signature, aggregate, valueObjects))
-                .collect(Collectors.toList());
+  public static List<TestCase> from(final CodeGenerationParameter aggregate,
+      List<CodeGenerationParameter> valueObjects) {
+    return aggregate.retrieveAllRelated(Label.ROUTE_SIGNATURE)
+        .map(signature -> new TestCase(signature, aggregate, valueObjects))
+        .collect(Collectors.toList());
+  }
+
+  private TestCase(final CodeGenerationParameter signature, final CodeGenerationParameter aggregate,
+      List<CodeGenerationParameter> valueObjects) {
+    final TestDataValueGenerator.TestDataValues testDataValues = TestDataValueGenerator
+        .with(TEST_DATA_SET_SIZE, "data", aggregate, valueObjects).generate();
+
+    final String dataObjectType = JavaTemplateStandard.DATA_OBJECT.resolveClassname(aggregate.value);
+    this.methodName = signature.value;
+
+    this.urlPathCount = urlPathCountFor(signature.retrieveRelatedValue(Label.ROUTE_PATH));
+    this.collectionMutation = retrieveSignatureCollectionMutation(signature, aggregate);
+    this.isModelHasSelfDescribingEvents = isModelHasSelfDescribingEvents(aggregate);
+    
+    this.dataDeclaration = DataDeclaration.generate(signature.value, aggregate, valueObjects, testDataValues);
+    this.rootMethod = signature.retrieveRelatedValue(Label.ROUTE_METHOD).toLowerCase(Locale.ROOT);
+    this.preliminaryStatements.addAll(PreliminaryStatement.with(aggregate.retrieveRelatedValue(Label.URI_ROOT),
+        dataObjectType, rootPath(signature, aggregate), rootMethod));
+    this.statements.addAll(TestStatement.with(rootPath(signature, aggregate), rootMethod, aggregate, valueObjects, testDataValues));
+  }
+
+  private CollectionMutation retrieveSignatureCollectionMutation(CodeGenerationParameter signature, CodeGenerationParameter aggregate) {
+    CollectionMutation result = CollectionMutation.NONE;
+    final Optional<CodeGenerationParameter> aggregateMethod = aggregate.retrieveAllRelated(Label.AGGREGATE_METHOD)
+        .filter(method -> method.value.equals(signature.value))
+        .findFirst();
+
+    if (aggregateMethod.isPresent()) {
+      final String mutation = aggregateMethod.get()
+          .retrieveOneRelated(Label.METHOD_PARAMETER)
+          .retrieveRelatedValue(Label.COLLECTION_MUTATION);
+      if (!mutation.isEmpty())
+        result = CollectionMutation.valueOf(mutation);
     }
+    return result;
+  }
 
-    private TestCase(final CodeGenerationParameter signature, final CodeGenerationParameter aggregate,
-                     List<CodeGenerationParameter> valueObjects) {
-        final TestDataValueGenerator.TestDataValues testDataValues = TestDataValueGenerator
-                .with(TEST_DATA_SET_SIZE, "data", aggregate, valueObjects).generate();
+  private String rootPath(CodeGenerationParameter signature, CodeGenerationParameter aggregate) {
+    String uriRoot = aggregate.retrieveRelatedValue(Label.URI_ROOT);
+    return signature.retrieveRelatedValue(Label.ROUTE_PATH).startsWith(uriRoot)
+        ? signature.retrieveRelatedValue(Label.ROUTE_PATH)
+        : uriRoot + signature.retrieveRelatedValue(Label.ROUTE_PATH);
+  }
 
-        final String dataObjectType = JavaTemplateStandard.DATA_OBJECT.resolveClassname(aggregate.value);
-        this.urlPathCount = urlPathCountFor(signature.retrieveRelatedValue(Label.ROUTE_PATH));
-
-        final Optional<CodeGenerationParameter> aggregateMethod = aggregate.retrieveAllRelated(Label.AGGREGATE_METHOD)
-            .filter(m -> m.value.equals(signature.value))
-            .findFirst();
-        aggregateMethod.ifPresent(method -> {
-            final String mutation = method
-                .retrieveOneRelated(Label.METHOD_PARAMETER)
-                .retrieveRelatedValue(Label.COLLECTION_MUTATION);
-            if(!mutation.isEmpty())
-                this.collectionMutation = CollectionMutation.valueOf(mutation);
-        });
-
-        this.isModelHasSelfDescribingEvents = isModelHasSelfDescribingEvents(aggregate);
-        this.methodName = signature.value;
-        this.dataDeclaration = DataDeclaration.generate(signature.value, aggregate, valueObjects, testDataValues);
-        this.rootMethod = signature.retrieveRelatedValue(Label.ROUTE_METHOD).toLowerCase(Locale.ROOT);
-        this.preliminaryStatements.addAll(PreliminaryStatement.with(aggregate.retrieveRelatedValue(Label.URI_ROOT), dataObjectType, rootPath(signature, aggregate), rootMethod));
-        this.statements.addAll(TestStatement.with(rootPath(signature, aggregate), rootMethod, aggregate, valueObjects, testDataValues));
+  private int urlPathCountFor(String url) {
+    Pattern pattern = Pattern.compile("\\{(.*?)\\}");
+    Matcher matcher = pattern.matcher(url);
+    int count = 0;
+    while (matcher.find()) {
+      count++;
     }
+    return count;
+  }
 
-    private String rootPath(CodeGenerationParameter signature, CodeGenerationParameter aggregate) {
-        String uriRoot = aggregate.retrieveRelatedValue(Label.URI_ROOT);
-        return signature.retrieveRelatedValue(Label.ROUTE_PATH).startsWith(uriRoot) ? signature.retrieveRelatedValue(Label.ROUTE_PATH) : uriRoot + signature.retrieveRelatedValue(Label.ROUTE_PATH);
-    }
+  private boolean isModelHasSelfDescribingEvents(CodeGenerationParameter aggregate) {
+    return aggregate.retrieveAllRelated(Label.DOMAIN_EVENT)
+        .filter(domainEvent -> domainEvent.retrieveAllRelated(Label.STATE_FIELD).count() == 1).count() > 0;
+  }
 
-    private int urlPathCountFor(String url) {
-      Pattern pattern = Pattern.compile("\\{(.*?)\\}");
-      Matcher matcher = pattern.matcher(url);
-      int count = 0;
-      while (matcher.find()) {
-          count++;
-      }
-      return count;
-    }
+  public String getMethodName() {
+    return methodName;
+  }
 
-		private boolean isModelHasSelfDescribingEvents(CodeGenerationParameter aggregate) {
-      return aggregate.retrieveAllRelated(Label.DOMAIN_EVENT)
-      .filter(domainEvent -> domainEvent.retrieveAllRelated(Label.STATE_FIELD).count()==1).count() > 0;
-    }
+  public String getRootMethod() {
+    return rootMethod;
+  }
 
-    public String getMethodName() {
-        return methodName;
-    }
+  public boolean isRootMethod() {
+    return !getRootMethod().equals("post");
+  }
 
-    public String getRootMethod() {
-        return rootMethod;
-    }
+  public boolean isDisabled() {
+    return this.urlPathCount > 1
+        || (this.collectionMutation != null && this.collectionMutation.isSingleParameterBased());
+  }
 
-    public boolean isRootMethod() {
-        return !getRootMethod().equals("post");
-    }
+  public boolean modelHasSelfDescribingEvents() {
+    return isModelHasSelfDescribingEvents;
+  }
 
-    public boolean isDisabled() {
-      return this.urlPathCount > 1 || (this.collectionMutation != null && this.collectionMutation.isSingleParameterBased());
-    }
+  public String getDataDeclaration() {
+    return dataDeclaration;
+  }
 
-		public boolean modelHasSelfDescribingEvents() {
-      return isModelHasSelfDescribingEvents;
-    }
+  public List<TestStatement> getStatements() {
+    return statements;
+  }
 
-    public String getDataDeclaration() {
-        return dataDeclaration;
-    }
-
-    public List<TestStatement> getStatements() {
-        return statements;
-    }
-
-    public List<String> getPreliminaryStatements() {
-        return preliminaryStatements;
-    }
+  public List<String> getPreliminaryStatements() {
+    return preliminaryStatements;
+  }
 }
