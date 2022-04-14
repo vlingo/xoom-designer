f13b74aec6fae8fefa138a8392f3f23544265675
Hamza JGUERIM <hamzajg@gmail.com>
2022-04-14 23:16:56 +0100

    Update platform UI..
    Signed-off-by: Hamza JGUERIM <hamzajg@gmail.com>

--- src/main/frontend/src/routes/platform.svelte
+++ src/main/frontend/src/routes/platform.svelte
@@ -9,11 +9,31 @@
 			options: [
 				{
 					name: 'Java',
+					value: 'Java',
 					disabled: false,
+          sdkVersions: [
+            {
+              name: "JDK 8",
+              value: "1.8"
+            },
+            {
+              name: "JDK 10",
+              value: "10"
+            }
+          ],
+           vlingoVersions: [
+             {
+               name: "1.9.3 (Stable)",
+               value: "1.9.3"
+             }
+           ]
 				},
 				{
 					name: 'Kotlin',
-					disabled: true
+					value: 'Kotlin',
+					disabled: true,
+          sdkVersions: [],
+          vlingoVersions: []
 				}
 			],
 			default: 'Java',
@@ -23,11 +43,27 @@
 			options: [
 				{
 					name: 'C#',
-					disabled: false
+					value: 'C_SHARP',
+					disabled: false,
+					sdkVersions: [
+						{
+							name: "Net 6.0",
+							value: "net6.0"
+						}
+					],
+          vlingoVersions: [
+            {
+              name: "1.9.3 (Stable)",
+              value: "1.9.3"
+            }
+          ]
 				},
 				{
 					name: 'F#',
-					disabled: true
+					value: 'F_SHARP',
+					disabled: true,
+          sdkVersions: [],
+          vlingoVersions: []
 				}
 			],
 			default: 'C#'
@@ -36,6 +72,8 @@
 
 	let value = $settings.platformSettings && $settings.platformSettings.platform === ".NET";
   let lang = $settings.platformSettings && $settings.platformSettings.lang;
+  let sdkVersion = $settings.platformSettings && $settings.platformSettings.sdkVersion;
+  let vlingoVersion = $settings.platformSettings && $settings.platformSettings.vlingoVersion;
 	let options = platforms.reduce((acc, cur) => {
 		if ($settings.platformSettings && $settings.platformSettings.platform === cur.name) acc = [...cur.options]
 		return acc;
@@ -43,7 +81,9 @@
 
 	$: $settings.platformSettings = {
 		platform: value ? '.NET' : 'JVM',
-		lang
+		lang,
+		sdkVersion,
+		vlingoVersion
 	};
 
 	function updateOptions() {
@@ -51,7 +91,7 @@
 		const platform = platforms.find(p => p.name === v)
 		if (platform) {
 			options = platform.options
-			lang = platform.options.some(o => o.name === lang) ? lang : platform.default 
+			lang = platform.options.some(o => o.value === lang) ? lang : platform.default
 		}
 	}
 
@@ -66,16 +106,37 @@
 <CardForm title="Platform" next="context">
   <div class="vl-switch d-flex justify-center mb-8 mt-8">
     <b>JVM</b>
-    <!-- <Switch class="mr-4 ml-8" bind:checked={value} inset></Switch>
-    <b>.NET</b> -->
+    <Switch class="mr-4 ml-8" bind:checked={value} inset></Switch>
+    <b>.NET</b>
   </div>
 
   <div class="d-flex justify-center mb-8">
+    <fieldset class="mb-8">
+  	<legend>Language: </legend>
     {#each options as option (option)}
       <div class="ml-6 mr-6">
-				<Radio bind:group={lang} disabled={option.disabled} value={option.name}>{option.name}</Radio>
+				<Radio bind:group={lang} disabled={option.disabled} value={option.value}>{option.name}</Radio>
 			</div>
+    {#if !option.disabled}
+    <fieldset>
+  	<legend>SDK Version: </legend>
+		{#each option.sdkVersions as sdk (sdk)}
+      <div class="ml-6 mr-6">
+        <Radio bind:group={sdkVersion} value={sdk.value}>{sdk.name}</Radio> <br />
+      </div>
     {/each}
+    </fieldset>
+    <fieldset>
+  	<legend>VLINGO Version: </legend>
+		{#each option.vlingoVersions as vlingo (vlingo)}
+      <div class="ml-6 mr-6">
+        <Radio bind:group={vlingoVersion} value={vlingo.value}>{vlingo.name}</Radio> <br />
+      </div>
+    {/each}
+    </fieldset>
+    {/if}
+    {/each}
+    </fieldset>
   </div>
 </CardForm>
 
