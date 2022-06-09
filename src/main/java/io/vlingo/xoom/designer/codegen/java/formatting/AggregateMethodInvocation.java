30cff30616ac92d121fef9d46b17754a077b0952
Hamza JGUERIM <hamzajg@gmail.com>
2022-06-09 21:17:47 +0100

    Fix consumer exchange generating with value object.
    Signed-off-by: Hamza JGUERIM <hamzajg@gmail.com>

--- src/main/java/io/vlingo/xoom/designer/codegen/java/formatting/AggregateMethodInvocation.java
+++ src/main/java/io/vlingo/xoom/designer/codegen/java/formatting/AggregateMethodInvocation.java
@@ -16,6 +16,7 @@ import io.vlingo.xoom.designer.codegen.java.model.valueobject.ValueObjectDetail;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -28,6 +29,12 @@ public class AggregateMethodInvocation implements Formatters.Arguments {
   private final ParametersOwner parametersOwner;
   private static final String FIELD_ACCESS_PATTERN = "%s.%s";
   private static final String SCALAR_TYPED_SINGLETON_COLLECTION_PATTERN = "%s.%s.stream().findFirst().orElse(null)";
+  private CodeGenerationParameter valueObject;
+
+  public AggregateMethodInvocation(String stageVariableName, ParametersOwner consumedEvent, CodeGenerationParameter valueObject) {
+    this(stageVariableName, consumedEvent);
+    this.valueObject = valueObject;
+  }
 
   public static AggregateMethodInvocation accessingParametersFromDataObject(final String stageVariableName) {
     return new AggregateMethodInvocation(stageVariableName, ParametersOwner.DATA_OBJECT);
@@ -37,23 +44,41 @@ public class AggregateMethodInvocation implements Formatters.Arguments {
     return new AggregateMethodInvocation(stageVariableName, ParametersOwner.CONSUMED_EVENT);
   }
 
+  public static AggregateMethodInvocation accessingValueObjectParametersFromConsumedEvent(String stageVariableName,
+                                                                                          CodeGenerationParameter valueObject) {
+    return new AggregateMethodInvocation(stageVariableName, ParametersOwner.CONSUMED_EVENT, valueObject);
+  }
+
   public AggregateMethodInvocation(final String stageVariableName) {
     this(stageVariableName, ParametersOwner.NONE);
   }
 
-  public AggregateMethodInvocation(final String stageVariableName,
-                                   final ParametersOwner parametersOwner) {
+  public AggregateMethodInvocation(final String stageVariableName, final ParametersOwner parametersOwner) {
     this.stageVariableName = stageVariableName;
     this.parametersOwner = parametersOwner;
   }
 
   @Override
   public String format(final CodeGenerationParameter method, final MethodScope scope) {
-    final List<String> args = scope.isStatic() ?
-            Arrays.asList(stageVariableName) : Arrays.asList();
+    final List<String> args = scope.isStatic() ? Collections.singletonList(stageVariableName) : Collections.emptyList();
+    if (valueObject != null) {
+      String collect = Stream.of(getCollect(formatMethodParameters(method).get(0), valueObject, scope))
+              .flatMap(Collection::stream)
+              .collect(Collectors.joining(", "));
+      if(scope.isStatic())
+        return String.format("%s, %s.from(%s)", stageVariableName, valueObject.value, collect);
 
-    return Stream.of(args, formatMethodParameters(method))
-            .flatMap(Collection::stream).collect(Collectors.joining(", "));
+      return String.format("%s.from(%s)", valueObject.value, collect);
+    } else
+      return Stream.of(args, formatMethodParameters(method))
+              .flatMap(Collection::stream)
+              .collect(Collectors.joining(", "));
+  }
+
+  private List<String> getCollect(final String fieldPath, final CodeGenerationParameter valueObject, MethodScope scope) {
+    return Arrays.stream(Formatters.Arguments.DATA_OBJECT_CONSTRUCTOR_INVOCATION.format(valueObject, scope).split(", "))
+            .map(param -> Arrays.stream(String.format("%s.%s", fieldPath, param).split("\\.")).distinct().collect(Collectors.joining(".")))
+            .collect(toList());
   }
 
   private List<String> formatMethodParameters(final CodeGenerationParameter method) {
